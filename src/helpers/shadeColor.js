d61ac008dc45ba798ba0a8e783f9b6f3177ef64f
Michael Schmidt <msrd0000@gmail.com>
2022-04-28 14:10:03 +0200

    Refactored shadeColor (#61)

--- src/helpers/shadeColor.js
+++ src/helpers/shadeColor.js
@@ -1,21 +1,26 @@
 // From https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
 
+/**
+ * Lightens (percentage > 0) or darkens (percentage < 0) the given hex color.
+ *
+ * The color has to be in the form `#RRGGBB`.
+ *
+ * @param {string} color
+ * @param {number} percent
+ * @returns {`#${string}`}
+ */
 const shadeColor = (color, percent) => {
   let R = parseInt(color.substring(1, 3), 16);
   let G = parseInt(color.substring(3, 5), 16);
   let B = parseInt(color.substring(5, 7), 16);
 
-  R = parseInt((R * (100 + percent)) / 100, 10);
-  G = parseInt((G * (100 + percent)) / 100, 10);
-  B = parseInt((B * (100 + percent)) / 100, 10);
+  R = Math.min(Math.round(R * (1 + percent / 100)), 255);
+  G = Math.min(Math.round(G * (1 + percent / 100)), 255);
+  B = Math.min(Math.round(B * (1 + percent / 100)), 255);
 
-  R = (R < 255) ? R : 255;
-  G = (G < 255) ? G : 255;
-  B = (B < 255) ? B : 255;
-
-  const RR = ((R.toString(16).length === 1) ? `0${R.toString(16)}` : R.toString(16));
-  const GG = ((G.toString(16).length === 1) ? `0${G.toString(16)}` : G.toString(16));
-  const BB = ((B.toString(16).length === 1) ? `0${B.toString(16)}` : B.toString(16));
+  const RR = R.toString(16).padStart(2, '0');
+  const GG = G.toString(16).padStart(2, '0');
+  const BB = B.toString(16).padStart(2, '0');
 
   return `#${RR}${GG}${BB}`;
 };
