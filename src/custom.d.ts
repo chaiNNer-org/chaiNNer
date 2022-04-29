6e3a3298c1a31c34d486ae66f513fa83dd403dcb
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-04-29 18:24:22 +0200

    Added types for images

---
+++ src/custom.d.ts
@@ -0,0 +1,20 @@
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
