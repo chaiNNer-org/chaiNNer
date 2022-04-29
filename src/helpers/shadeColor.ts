c1fa67014ed0d24fd89597e8b13fd26f75121985
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-04-29 18:24:37 +0200

    WIP

--- src/helpers/shadeColor.ts
+++ src/helpers/shadeColor.ts
@@ -5,7 +5,7 @@
  *
  * The color has to be in the form `#RRGGBB`.
  */
-const shadeColor = (color:string, percent:number):`#${string}` => {
+const shadeColor = (color: string, percent: number): `#${string}` => {
   let R = parseInt(color.substring(1, 3), 16);
   let G = parseInt(color.substring(3, 5), 16);
   let B = parseInt(color.substring(5, 7), 16);
