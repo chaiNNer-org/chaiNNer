5f09fe092b14ad9c19f34b094e4f1dc03c424b46
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2022-04-29 17:20:52 -0400

    Download correct python build for M1 macbooks (#88)

--- src/downloads.js
+++ src/downloads.js
@@ -1,9 +1,15 @@
+import os from 'os';
+
+const cpus = os.cpus();
+const isM1 = cpus[0].model.includes('Apple M1');
+
 const downloads = {
   python: {
     linux:
       'https://github.com/indygreg/python-build-standalone/releases/download/20211017/cpython-3.9.7-x86_64-unknown-linux-gnu-install_only-20211017T1616.tar.gz',
-    darwin:
-      'https://github.com/indygreg/python-build-standalone/releases/download/20211017/cpython-3.9.7-x86_64-apple-darwin-install_only-20211017T1616.tar.gz',
+    darwin: isM1
+      ? 'https://github.com/indygreg/python-build-standalone/releases/download/20220318/cpython-3.9.11+20220318-aarch64-apple-darwin-install_only.tar.gz'
+      : 'https://github.com/indygreg/python-build-standalone/releases/download/20211017/cpython-3.9.7-x86_64-apple-darwin-install_only-20211017T1616.tar.gz',
     win32:
       'https://github.com/indygreg/python-build-standalone/releases/download/20211017/cpython-3.9.7-x86_64-pc-windows-msvc-shared-install_only-20211017T1616.tar.gz',
   },
