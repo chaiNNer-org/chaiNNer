3b7ae29be090c0d03eae20315778e94dc51ce0c1
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-04-30 20:10:41 +0200

    Use primise fs

--- src/setupIntegratedPython.ts
+++ src/setupIntegratedPython.ts
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 import decompress from 'decompress';
 import log from 'electron-log';
-import fs from 'fs';
+import fs from 'fs/promises';
 import Downloader from 'nodejs-file-downloader';
 import os from 'os';
 import path from 'path';
@@ -37,19 +37,23 @@ export const extractPython = async (
 ) => {
   const fileData = Array.from(await decompress(path.join(directory, '/python.tar.gz')));
   const totalFiles = fileData.length;
-  fileData.forEach((file, i) => {
-    const filePath = path.join(directory, file.path);
-    const fileDir = path.dirname(filePath);
-    fs.mkdirSync(fileDir, { recursive: true });
-    fs.writeFileSync(filePath, file.data);
-    const percentageComplete = (i / totalFiles) * 100;
-    onProgress(percentageComplete);
-  });
-  fs.rmSync(path.join(directory, '/python.tar.gz'));
+  let doneCounter = 0;
+  await Promise.all(
+    fileData.map(async (file) => {
+      const filePath = path.join(directory, file.path);
+      const fileDir = path.dirname(filePath);
+      await fs.mkdir(fileDir, { recursive: true });
+      await fs.writeFile(filePath, file.data);
+      const percentageComplete = (doneCounter / totalFiles) * 100;
+      doneCounter += 1;
+      onProgress(percentageComplete);
+    })
+  );
+  await fs.rm(path.join(directory, '/python.tar.gz'));
   if (['linux', 'darwin'].includes(os.platform())) {
     try {
       log.info('Granting perms for integrated python...');
-      fs.chmod(pythonPath, 0o7777, (error) => {
+      await fs.chmod(pythonPath, 0o7777).catch((error) => {
         log.error(error);
       });
     } catch (error) {
