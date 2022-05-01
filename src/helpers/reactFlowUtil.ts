c304836a19c2fd8b7a647dd5d5f92fa34280e2c2
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-05-01 15:56:12 +0200

    Migrated GlobalNodeState to TS

---
+++ src/helpers/reactFlowUtil.ts
@@ -0,0 +1,10 @@
+import { XYPosition } from 'react-flow-renderer';
+
+// eslint-disable-next-line import/prefer-default-export
+export const snapToGrid = (
+  position: Readonly<XYPosition>,
+  snapToGridAmount: number
+): XYPosition => ({
+  x: position.x - (position.x % snapToGridAmount),
+  y: position.y - (position.y % snapToGridAmount),
+});
