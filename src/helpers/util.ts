12840f0766fd044c5c3c0d03fb2e67a1eaa337e8
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-05-03 11:49:14 +0200

    Minor improvements to alert box

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,9 +1,12 @@
 import { constants } from 'fs';
 import fs from 'fs/promises';
 
-// eslint-disable-next-line import/prefer-default-export
 export const checkFileExists = (file: string): Promise<boolean> =>
   fs.access(file, constants.F_OK).then(
     () => true,
     () => false
   );
+
+export const assertNever = (value: never): never => {
+  throw new Error(`Unreachable code path. The value ${String(value)} is invalid.`);
+};
