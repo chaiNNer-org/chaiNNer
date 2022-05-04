0311c1efbce04bd33645c9b565f6dfd47002812e
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2022-05-03 23:41:58 -0400

    Refactor error message boxes (#118)
    * Refactor error box (broken)
    
    * typescript thing
    
    * Fix error box
    
    * Minor improvements to alert box (#113)
    
    * Minor improvements to alert box
    
    * Better props type
    
    * Replace main page alert box
    
    * Update critical alert message
    
    * Remove temporary failure test
    
    Co-authored-by: Michael Schmidt <mitchi5000.ms@googlemail.com>

---
+++ src/helpers/contexts/AlertBoxContext.tsx
@@ -0,0 +1,131 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  HStack,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { app, clipboard } from 'electron';
+import React, { createContext, useMemo, useRef, useState } from 'react';
+import { assertNever } from '../util';
+
+interface AlertBox {
+  showMessageBox: (newAlertType: AlertType, newTitle: string | null, newMessage: string) => void;
+}
+
+export enum AlertType {
+  INFO = 'Info',
+  WARN = 'Warning',
+  ERROR = 'Error',
+  CRIT_ERROR = 'Critical Error',
+}
+
+export const AlertBoxContext = createContext<Readonly<AlertBox>>({} as AlertBox);
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const AlertBoxProvider = ({ children }: React.PropsWithChildren<{}>) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [alertType, setAlertType] = useState<AlertType>(AlertType.INFO);
+  const [title, setTitle] = useState<string | null>(null);
+  const [message, setMessage] = useState<string>('');
+  const cancelRef = useRef<HTMLButtonElement>(null);
+
+  const showMessageBox = (newAlertType: AlertType, newTitle: string | null, newMessage: string) => {
+    setAlertType(newAlertType);
+    setTitle(newTitle ?? newAlertType);
+    setMessage(newMessage);
+    onOpen();
+  };
+
+  const getButtons = (type: AlertType): JSX.Element => {
+    switch (type) {
+      case AlertType.INFO:
+        return (
+          <Button
+            ref={cancelRef}
+            onClick={onClose}
+          >
+            OK
+          </Button>
+        );
+      case AlertType.WARN:
+        return (
+          <Button
+            ref={cancelRef}
+            onClick={onClose}
+          >
+            OK
+          </Button>
+        );
+      case AlertType.ERROR:
+        return (
+          <>
+            <Button
+              onClick={() => {
+                clipboard.writeText(message);
+              }}
+            >
+              Copy to Clipboard
+            </Button>
+            <Button
+              ref={cancelRef}
+              onClick={onClose}
+            >
+              OK
+            </Button>
+          </>
+        );
+      case AlertType.CRIT_ERROR:
+        return (
+          <Button
+            colorScheme="red"
+            ml={3}
+            onClick={() => {
+              window.close();
+              app.quit();
+            }}
+            ref={cancelRef}
+          >
+            Exit Application
+          </Button>
+        );
+      default:
+        return assertNever(type);
+    }
+  };
+
+  const buttons = useMemo(() => getButtons(alertType), [alertType, cancelRef, message, onClose]);
+
+  const closeApp = () => {
+    window.close();
+    app.quit();
+  };
+
+  return (
+    <AlertBoxContext.Provider value={{ showMessageBox }}>
+      <AlertDialog
+        isCentered
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={alertType === AlertType.CRIT_ERROR ? closeApp : onClose}
+      >
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+          <AlertDialogHeader>{title}</AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody whiteSpace="pre-wrap">{message}</AlertDialogBody>
+          <AlertDialogFooter>
+            <HStack>{buttons}</HStack>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      {children}
+    </AlertBoxContext.Provider>
+  );
+};
