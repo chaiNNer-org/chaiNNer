0bcf5d04d41f2d4207dc4985be710ed9131150c1
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-05-11 13:40:15 +0200

    Memoize alert box context

--- src/helpers/contexts/AlertBoxContext.tsx
+++ src/helpers/contexts/AlertBoxContext.tsx
@@ -11,7 +11,7 @@ import {
     useDisclosure,
 } from '@chakra-ui/react';
 import { app, clipboard } from 'electron';
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import { createContext } from 'use-context-selector';
 import { assertNever } from '../util';
 
@@ -36,16 +36,16 @@ export const AlertBoxProvider = ({ children }: React.PropsWithChildren<{}>) => {
     const [message, setMessage] = useState<string>('');
     const cancelRef = useRef<HTMLButtonElement>(null);
 
-    const showMessageBox = (
-        newAlertType: AlertType,
-        newTitle: string | null,
-        newMessage: string
-    ) => {
-        setAlertType(newAlertType);
-        setTitle(newTitle ?? newAlertType);
-        setMessage(newMessage);
-        onOpen();
-    };
+    const showMessageBox = useCallback(
+        // eslint-disable-next-line @typescript-eslint/no-shadow
+        (alertType: AlertType, title: string | null, message: string) => {
+            setAlertType(alertType);
+            setTitle(title ?? alertType);
+            setMessage(message);
+            onOpen();
+        },
+        [setAlertType, setTitle, setMessage, onOpen]
+    );
 
     const getButtons = (type: AlertType): JSX.Element => {
         switch (type) {
@@ -111,8 +111,11 @@ export const AlertBoxProvider = ({ children }: React.PropsWithChildren<{}>) => {
         app.quit();
     };
 
+    let value: AlertBox = { showMessageBox };
+    value = useMemo(() => value, Object.values(value));
+
     return (
-        <AlertBoxContext.Provider value={{ showMessageBox }}>
+        <AlertBoxContext.Provider value={value}>
             <AlertDialog
                 isCentered
                 isOpen={isOpen}
