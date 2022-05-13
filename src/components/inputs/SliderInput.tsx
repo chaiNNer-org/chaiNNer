71ef55cf7a3c363fa026f432e5ffa39255cfc13e
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-05-13 17:47:22 +0200

    Clearer interpolation slider (#213)
    * WIP
    
    * Final

--- src/components/inputs/SliderInput.tsx
+++ src/components/inputs/SliderInput.tsx
@@ -11,6 +11,7 @@ import {
     SliderTrack,
     Text,
     Tooltip,
+    VStack,
 } from '@chakra-ui/react';
 import { memo, useEffect, useState } from 'react';
 import { InputProps } from './props';
@@ -20,10 +21,33 @@ interface SliderInputProps extends InputProps {
     max: number;
     def?: number;
     accentColor: string;
+    hideEnds?: boolean;
+    noteExpression?: string;
 }
 
+const tryEvaluate = (expression: string, args: Record<string, unknown>): string | undefined => {
+    try {
+        return String(
+            // eslint-disable-next-line @typescript-eslint/no-implied-eval
+            new Function(...Object.keys(args), `return ${expression};`)(...Object.values(args))
+        );
+    } catch (error) {
+        return undefined;
+    }
+};
+
 const SliderInput = memo(
-    ({ index, useInputData, def, min, max, accentColor, isLocked }: SliderInputProps) => {
+    ({
+        index,
+        useInputData,
+        def,
+        min,
+        max,
+        hideEnds,
+        noteExpression,
+        accentColor,
+        isLocked,
+    }: SliderInputProps) => {
         const [input, setInput] = useInputData<number>(index);
         const [sliderValue, setSliderValue] = useState(input ?? def);
         const [showTooltip, setShowTooltip] = useState(false);
@@ -32,67 +56,75 @@ const SliderInput = memo(
             setSliderValue(input);
         }, [input]);
 
+        const expr = noteExpression
+            ? tryEvaluate(noteExpression, { min, max, value: sliderValue ?? def })
+            : undefined;
+        const filled = !expr;
+
         return (
-            <HStack w="full">
-                <Text fontSize="xs">{min}</Text>
-                <Slider
-                    defaultValue={def}
-                    focusThumbOnChange={false}
-                    isDisabled={isLocked}
-                    max={max}
-                    min={min}
-                    step={1}
-                    value={sliderValue ?? def}
-                    onChange={setSliderValue}
-                    onChangeEnd={setInput}
-                    onMouseEnter={() => setShowTooltip(true)}
-                    onMouseLeave={() => setShowTooltip(false)}
-                >
-                    <SliderTrack>
-                        <SliderFilledTrack bg={accentColor} />
-                    </SliderTrack>
-                    <Tooltip
-                        hasArrow
-                        bg={accentColor}
-                        borderRadius={8}
-                        color="white"
-                        isOpen={showTooltip}
-                        label={sliderValue}
-                        placement="top"
-                        px={2}
-                        py={1}
+            <VStack w="full">
+                <HStack w="full">
+                    {!hideEnds && <Text fontSize="xs">{min}</Text>}
+                    <Slider
+                        defaultValue={def}
+                        focusThumbOnChange={false}
+                        isDisabled={isLocked}
+                        max={max}
+                        min={min}
+                        step={1}
+                        value={sliderValue ?? def}
+                        onChange={setSliderValue}
+                        onChangeEnd={setInput}
+                        onMouseEnter={() => setShowTooltip(true)}
+                        onMouseLeave={() => setShowTooltip(false)}
                     >
-                        <SliderThumb />
-                    </Tooltip>
-                </Slider>
-                <Text fontSize="xs">{max}</Text>
-                <NumberInput
-                    className="nodrag"
-                    defaultValue={def}
-                    draggable={false}
-                    // precision={precision}
-                    isDisabled={isLocked}
-                    max={max}
-                    min={min}
-                    placeholder={def !== undefined ? String(def) : undefined}
-                    size="xs"
-                    step={1}
-                    value={sliderValue ?? def}
-                    onChange={(_, v) => {
-                        setInput(Math.min(Math.max(v, min), max));
-                    }}
-                >
-                    <NumberInputField
-                        m={0}
-                        p={1}
-                        w="3.1rem"
-                    />
-                    <NumberInputStepper w={4}>
-                        <NumberIncrementStepper />
-                        <NumberDecrementStepper />
-                    </NumberInputStepper>
-                </NumberInput>
-            </HStack>
+                        <SliderTrack>
+                            {filled ? <SliderFilledTrack bg={accentColor} /> : <SliderTrack />}
+                        </SliderTrack>
+                        <Tooltip
+                            hasArrow
+                            bg={accentColor}
+                            borderRadius={8}
+                            color="white"
+                            isOpen={showTooltip}
+                            label={sliderValue}
+                            placement="top"
+                            px={2}
+                            py={1}
+                        >
+                            <SliderThumb />
+                        </Tooltip>
+                    </Slider>
+                    {!hideEnds && <Text fontSize="xs">{max}</Text>}
+                    <NumberInput
+                        className="nodrag"
+                        defaultValue={def}
+                        draggable={false}
+                        // precision={precision}
+                        isDisabled={isLocked}
+                        max={max}
+                        min={min}
+                        placeholder={def !== undefined ? String(def) : undefined}
+                        size="xs"
+                        step={1}
+                        value={sliderValue ?? def}
+                        onChange={(_, v) => {
+                            setInput(Math.min(Math.max(v, min), max));
+                        }}
+                    >
+                        <NumberInputField
+                            m={0}
+                            p={1}
+                            w="3.1rem"
+                        />
+                        <NumberInputStepper w={4}>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                        </NumberInputStepper>
+                    </NumberInput>
+                </HStack>
+                {expr && <Text fontSize="xs">{expr}</Text>}
+            </VStack>
         );
     }
 );
