36d8c2a4ef4b69681ed20933f4473cc603c5adb8
Joey Ballentine <joeyjballentine@gmail.com>
2022-05-22 17:53:08 -0400

    Fix CI issues (hopefully)

--- backend/nodes/image_nodes.py
+++ backend/nodes/image_nodes.py
@@ -204,8 +204,8 @@ class ImOpenNode(NodeBase):
 
         if status:
             if platform.system() == "Darwin":  # macOS
-                subprocess.call(("open", temp_save_dir))
+                subprocess.call(("open", temp_save_dir))  # type: ignore
             elif platform.system() == "Windows":  # Windows
-                os.startfile(temp_save_dir)
+                os.startfile(temp_save_dir)  # type: ignore
             else:  # linux variants
-                subprocess.call(("xdg-open", temp_save_dir))
+                subprocess.call(("xdg-open", temp_save_dir))  # type: ignore
