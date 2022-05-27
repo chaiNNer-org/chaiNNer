ab8fddc0d6804c82e0dcb17c7ade53e519791c3b
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-05-27 23:04:27 +0200

    Improved search

--- src/renderer/components/NodeSelectorPanel/FavoritesAccordionItem.tsx
+++ src/renderer/components/NodeSelectorPanel/FavoritesAccordionItem.tsx
@@ -16,73 +16,75 @@ import { TextBox } from './TextBox';
 
 interface FavoritesAccordionItemProps {
     favoriteNodes: NodeSchema[];
+    noFavorites: boolean;
     collapsed: boolean;
 }
 
-const FavoritesAccordionItem = memo(({ favoriteNodes, collapsed }: FavoritesAccordionItemProps) => {
-    return (
-        <AccordionItem>
-            <AccordionButton>
-                <HStack
-                    flex="1"
-                    h={6}
-                    textAlign="left"
-                    verticalAlign="center"
+const FavoritesAccordionItem = memo(
+    ({ favoriteNodes, noFavorites, collapsed }: FavoritesAccordionItemProps) => {
+        return (
+            <AccordionItem>
+                <AccordionButton>
+                    <HStack
+                        flex="1"
+                        h={6}
+                        textAlign="left"
+                        verticalAlign="center"
+                    >
+                        <Center>
+                            <StarIcon color="yellow.500" />
+                        </Center>
+                        {!collapsed && (
+                            <Heading
+                                size="5xl"
+                                textOverflow="clip"
+                                whiteSpace="nowrap"
+                            >
+                                Favorites
+                            </Heading>
+                        )}
+                    </HStack>
+                    <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel
+                    pb={2.5}
+                    pt={0}
                 >
-                    <Center>
-                        <StarIcon color="yellow.500" />
-                    </Center>
-                    {!collapsed && (
-                        <Heading
-                            size="5xl"
-                            textOverflow="clip"
-                            whiteSpace="nowrap"
-                        >
-                            Favorites
-                        </Heading>
-                    )}
-                </HStack>
-                <AccordionIcon />
-            </AccordionButton>
-            <AccordionPanel
-                pb={2.5}
-                pt={0}
-            >
-                <Box>
-                    {favoriteNodes.length > 0 ? (
-                        favoriteNodes
-                            .filter((e) => e.nodeType !== 'iteratorHelper')
-                            .map((node) => (
+                    <Box>
+                        {!noFavorites ? (
+                            favoriteNodes.map((node) => (
                                 <RepresentativeNodeWrapper
                                     collapsed={collapsed}
                                     key={node.name}
                                     node={node}
                                 />
                             ))
-                    ) : (
-                        <TextBox
-                            collapsed={collapsed}
-                            text="No Favorites."
-                            toolTip={
-                                collapsed ? (
-                                    <>
-                                        Add Favorites by right-clicking nodes and selecting{' '}
-                                        <em>Add to Favorites</em>.
-                                    </>
-                                ) : (
-                                    <>
-                                        Add Favorites by hovering over nodes and clicking the{' '}
-                                        <StarIcon style={{ verticalAlign: 'baseline' }} /> icon, or
-                                        by right-clicking and selecting <em>Add to Favorites</em>.
-                                    </>
-                                )
-                            }
-                        />
-                    )}
-                </Box>
-            </AccordionPanel>
-        </AccordionItem>
-    );
-});
+                        ) : (
+                            <TextBox
+                                collapsed={collapsed}
+                                text="No Favorites."
+                                toolTip={
+                                    collapsed ? (
+                                        <>
+                                            Add Favorites by right-clicking nodes and selecting{' '}
+                                            <em>Add to Favorites</em>.
+                                        </>
+                                    ) : (
+                                        <>
+                                            Add Favorites by hovering over nodes and clicking the{' '}
+                                            <StarIcon style={{ verticalAlign: 'baseline' }} /> icon,
+                                            or by right-clicking and selecting{' '}
+                                            <em>Add to Favorites</em>.
+                                        </>
+                                    )
+                                }
+                            />
+                        )}
+                    </Box>
+                </AccordionPanel>
+            </AccordionItem>
+        );
+    }
+);
 
 export { FavoritesAccordionItem };
