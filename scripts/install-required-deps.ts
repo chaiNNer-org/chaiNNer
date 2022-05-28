e367dccf3df5e6c96025b2201f4d4205980bc7ae
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-05-28 16:52:53 +0200

    Better errors

--- scripts/install-required-deps.ts
+++ scripts/install-required-deps.ts
@@ -2,26 +2,30 @@
 import { spawn } from 'child_process';
 import { requiredDependencies } from '../src/common/dependencies';
 
-const command = spawn('python', [
-    '-m',
-    'pip',
-    ...requiredDependencies.map((d) => `${d.packageName}==${d.version}`),
-    '--disable-pip-version-check',
-]);
+try {
+    const command = spawn('python', [
+        '-m',
+        'pip',
+        ...requiredDependencies.map((d) => `${d.packageName}==${d.version}`),
+        '--disable-pip-version-check',
+    ]);
 
-command.stdout.on('data', (data: unknown) => {
-    console.log(data);
-});
+    command.stdout.on('data', (data: unknown) => {
+        console.log(String(data));
+    });
 
-command.stderr.on('data', (data: unknown) => {
-    console.error(data);
-});
+    command.stderr.on('data', (data: unknown) => {
+        console.error(String(data));
+    });
 
-command.on('error', (error) => {
-    console.error(error);
-    process.exit(1);
-});
+    command.on('error', (error) => {
+        console.error(error);
+        process.exit(1);
+    });
 
-command.on('close', (code) => {
-    process.exit(code ?? 1);
-});
+    command.on('close', (code) => {
+        process.exit(code ?? 1);
+    });
+} catch (error) {
+    console.error(String(error));
+}
