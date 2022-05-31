1859e08683786a11ef33302e4bc131e2b83cbc77
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-05-31 13:14:40 +0200

    Fixed env type hack

--- src/common/env.ts
+++ src/common/env.ts
@@ -3,6 +3,4 @@ import os from 'os';
 export const isMac = process.platform === 'darwin';
 export const isM1 = isMac && (os.cpus()[0]?.model.includes('Apple M1') ?? false);
 
-export const isRenderer =
-    typeof process !== 'undefined' &&
-    (process as unknown as { type: 'browser' | 'renderer' | 'worker' }).type === 'renderer';
+export const isRenderer = typeof process !== 'undefined' && process.type === 'renderer';
