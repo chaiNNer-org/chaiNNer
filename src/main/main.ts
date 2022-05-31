40d2ac93c659f9f14c087c10e1ddb01dc568c386
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-06-01 00:08:50 +0200

    Moved menu code into its own file (#388)

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,16 +1,7 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { ChildProcessWithoutNullStreams, exec as _exec, spawn } from 'child_process';
-import {
-    BrowserWindow,
-    Menu,
-    MenuItemConstructorOptions,
-    app,
-    dialog,
-    nativeTheme,
-    powerSaveBlocker,
-    shell,
-} from 'electron';
+import { BrowserWindow, app, dialog, nativeTheme, powerSaveBlocker, shell } from 'electron';
 import log from 'electron-log';
 import { readdirSync, rmSync } from 'fs';
 import { LocalStorage } from 'node-localstorage';
@@ -18,19 +9,19 @@ import os from 'os';
 import path from 'path';
 import portfinder from 'portfinder';
 import semver from 'semver';
-import { cpu, graphics } from 'systeminformation';
 import util from 'util';
 import { PythonInfo, WindowSize } from '../common/common-types';
 import { requiredDependencies } from '../common/dependencies';
-import { isMac } from '../common/env';
 import { runPipInstall, runPipList } from '../common/pip';
 import { getPythonInfo, setPythonInfo } from '../common/python';
 import { BrowserWindowWithSafeIpc, ipcMain } from '../common/safeIpc';
 import { SaveFile, openSaveFile } from '../common/SaveFile';
-import { checkFileExists, lazy } from '../common/util';
+import { checkFileExists } from '../common/util';
 import { getArguments } from './arguments';
+import { setMainMenu } from './menu';
 import { createNvidiaSmiVRamChecker, getNvidiaGpuName, getNvidiaSmi } from './nvidiaSmi';
 import { downloadPython, extractPython } from './setupIntegratedPython';
+import { getGpuInfo } from './systemInfo';
 import { hasUpdate } from './update';
 
 const exec = util.promisify(_exec);
@@ -115,9 +106,6 @@ ipcMain.handle('owns-backend', () => ownsBackend);
 
 let splash: BrowserWindowWithSafeIpc;
 let mainWindow: BrowserWindowWithSafeIpc;
-
-const getGpuInfo = lazy(() => graphics());
-const getCpuInfo = lazy(() => cpu());
 
 const registerEventHandlers = () => {
     ipcMain.handle('dir-select', (event, dirPath) =>
@@ -584,245 +572,6 @@ const doSplashScreenChecks = async () =>
         });
     });
 
-const setMainMenu = (openRecentRev: string[]) => {
-    const openRecent = openRecentRev.reverse();
-    const defaultPath = openRecent[0] ? path.dirname(openRecent[0]) : undefined;
-
-    const template = [
-        ...(isMac ? [{ role: 'appMenu' }] : []),
-        {
-            label: 'File',
-            submenu: [
-                {
-                    label: 'New',
-                    accelerator: 'CmdOrCtrl+N',
-                    click: () => {
-                        mainWindow.webContents.send('file-new');
-                    },
-                },
-                {
-                    label: 'Open...',
-                    accelerator: 'CmdOrCtrl+O',
-                    click: async () => {
-                        const {
-                            canceled,
-                            filePaths: [filepath],
-                        } = await dialog.showOpenDialog(mainWindow, {
-                            title: 'Open Chain File',
-                            defaultPath,
-                            filters: [{ name: 'Chain', extensions: ['chn'] }],
-                            properties: ['openFile'],
-                        });
-                        if (canceled) return;
-
-                        mainWindow.webContents.send('file-open', await openSaveFile(filepath));
-                    },
-                },
-                {
-                    label: 'Open Recent',
-                    submenu: [
-                        ...(openRecent.length === 0
-                            ? [
-                                  {
-                                      label: 'No entries',
-                                      enabled: false,
-                                  } as MenuItemConstructorOptions,
-                              ]
-                            : openRecent.map<MenuItemConstructorOptions>((filepath, i) => ({
-                                  label: filepath,
-                                  accelerator: i <= 9 ? `CmdOrCtrl+${i + 1}` : undefined,
-                                  click: async () => {
-                                      mainWindow.webContents.send(
-                                          'file-open',
-                                          await openSaveFile(filepath)
-                                      );
-                                  },
-                              }))),
-                        { type: 'separator' },
-                        {
-                            label: 'Clear Recently Opened',
-                            click: () => {
-                                mainWindow.webContents.send('clear-open-recent');
-                            },
-                        },
-                    ],
-                },
-                { type: 'separator' },
-                {
-                    label: 'Save',
-                    accelerator: 'CmdOrCtrl+S',
-                    click: () => {
-                        mainWindow.webContents.send('file-save');
-                    },
-                },
-                {
-                    label: 'Save As...',
-                    accelerator: 'CmdOrCtrl+Shift+S',
-                    click: () => {
-                        mainWindow.webContents.send('file-save-as');
-                    },
-                },
-                { type: 'separator' },
-                isMac ? { role: 'close' } : { role: 'quit' },
-            ],
-        },
-        {
-            label: 'Edit',
-            submenu: [
-                {
-                    label: 'Undo',
-                    accelerator: 'CmdOrCtrl+Z',
-                    registerAccelerator: false,
-                    click: () => {
-                        mainWindow.webContents.send('history-undo');
-                    },
-                },
-                {
-                    label: 'Redo',
-                    accelerator: 'CmdOrCtrl+Y',
-                    registerAccelerator: false,
-                    click: () => {
-                        mainWindow.webContents.send('history-redo');
-                    },
-                },
-                { type: 'separator' },
-                {
-                    label: 'Cut',
-                    accelerator: 'CmdOrCtrl+X',
-                    registerAccelerator: false,
-                    click: () => {
-                        mainWindow.webContents.send('cut');
-                    },
-                },
-                {
-                    label: 'Copy',
-                    accelerator: 'CmdOrCtrl+C',
-                    registerAccelerator: false,
-                    click: () => {
-                        mainWindow.webContents.send('copy');
-                    },
-                },
-                {
-                    label: 'Paste',
-                    accelerator: 'CmdOrCtrl+V',
-                    registerAccelerator: false,
-                    click: () => {
-                        mainWindow.webContents.send('paste');
-                    },
-                },
-                // ...(isMac ? [
-                //   { role: 'delete' },
-                //   { role: 'selectAll' },
-                //   { type: 'separator' },
-                //   {
-                //     label: 'Speech',
-                //     submenu: [
-                //       { role: 'startSpeaking' },
-                //       { role: 'stopSpeaking' },
-                //     ],
-                //   },
-                // ] : [
-                //   { role: 'delete' },
-                //   { type: 'separator' },
-                //   { role: 'selectAll' },
-                // ]),
-            ],
-        },
-        {
-            label: 'View',
-            submenu: [
-                { role: 'reload' },
-                { role: 'forceReload' },
-                { type: 'separator' },
-                { role: 'resetZoom' },
-                { role: 'zoomIn' },
-                { role: 'zoomOut' },
-                { type: 'separator' },
-                { role: 'togglefullscreen' },
-            ],
-        },
-        {
-            label: 'Window',
-            submenu: [
-                { role: 'minimize' },
-                { role: 'zoom' },
-                ...(isMac
-                    ? [
-                          { type: 'separator' },
-                          { role: 'front' },
-                          { type: 'separator' },
-                          { role: 'window' },
-                      ]
-                    : [{ role: 'close' }]),
-                ...(!app.isPackaged ? [{ type: 'separator' }, { role: 'toggleDevTools' }] : []),
-            ],
-        },
-        {
-            role: 'help',
-            submenu: [
-                {
-                    label: 'View README',
-                    click: async () => {
-                        await shell.openExternal(
-                            'https://github.com/joeyballentine/chaiNNer/blob/main/README.md'
-                        );
-                    },
-                },
-                {
-                    label: 'Open logs folder',
-                    click: async () => {
-                        await shell.openPath(app.getPath('logs'));
-                    },
-                },
-                {
-                    label: 'Get ESRGAN models',
-                    click: async () => {
-                        await shell.openExternal('https://upscale.wiki/wiki/Model_Database');
-                    },
-                },
-                {
-                    label: 'Convert ONNX models to NCNN',
-                    click: async () => {
-                        await shell.openExternal('https://convertmodel.com/');
-                    },
-                },
-                { type: 'separator' },
-                {
-                    label: 'Collect system information',
-                    click: async () => {
-                        const [cpuInfo, gpuInfo] = await Promise.all([getCpuInfo(), getGpuInfo()]);
-
-                        const information: Record<string, unknown> = {
-                            app: {
-                                version: app.getVersion(),
-                                packaged: app.isPackaged,
-                                path: app.getAppPath(),
-                            },
-                            process: {
-                                cwd: process.cwd(),
-                                argv: process.argv,
-                            },
-                            os: {
-                                version: os.version(),
-                                release: os.release(),
-                                arch: os.arch(),
-                                endianness: os.endianness(),
-                            },
-                            cpu: { ...cpuInfo },
-                            gpus: gpuInfo.controllers.map((c) => ({ ...c })),
-                        };
-
-                        mainWindow.webContents.send('show-collected-information', information);
-                    },
-                },
-            ],
-        },
-    ] as MenuItemConstructorOptions[];
-
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
-};
-
 const createWindow = async () => {
     // Create the browser window.
     mainWindow = new BrowserWindow({
@@ -843,8 +592,10 @@ const createWindow = async () => {
         show: false,
     }) as BrowserWindowWithSafeIpc;
 
-    setMainMenu([]);
-    ipcMain.on('update-open-recent-menu', (_, openRecent) => setMainMenu(openRecent));
+    setMainMenu({ mainWindow });
+    ipcMain.on('update-open-recent-menu', (_, openRecent) =>
+        setMainMenu({ mainWindow, openRecentRev: openRecent })
+    );
 
     await doSplashScreenChecks();
 
---
+++ src/main/menu.ts
@@ -0,0 +1,252 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Menu, MenuItemConstructorOptions, app, dialog, shell } from 'electron';
+import os from 'os';
+import path from 'path';
+import { isMac } from '../common/env';
+import { BrowserWindowWithSafeIpc } from '../common/safeIpc';
+import { openSaveFile } from '../common/SaveFile';
+import { getCpuInfo, getGpuInfo } from './systemInfo';
+
+export interface MainMenuArgs {
+    mainWindow: BrowserWindowWithSafeIpc;
+    openRecentRev?: string[];
+}
+
+export const setMainMenu = ({ mainWindow, openRecentRev = [] }: MainMenuArgs) => {
+    const openRecent = openRecentRev.reverse();
+    const defaultPath = openRecent[0] ? path.dirname(openRecent[0]) : undefined;
+
+    const template = [
+        ...(isMac ? [{ role: 'appMenu' }] : []),
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'New',
+                    accelerator: 'CmdOrCtrl+N',
+                    click: () => {
+                        mainWindow.webContents.send('file-new');
+                    },
+                },
+                {
+                    label: 'Open...',
+                    accelerator: 'CmdOrCtrl+O',
+                    click: async () => {
+                        const {
+                            canceled,
+                            filePaths: [filepath],
+                        } = await dialog.showOpenDialog(mainWindow, {
+                            title: 'Open Chain File',
+                            defaultPath,
+                            filters: [{ name: 'Chain', extensions: ['chn'] }],
+                            properties: ['openFile'],
+                        });
+                        if (canceled) return;
+
+                        mainWindow.webContents.send('file-open', await openSaveFile(filepath));
+                    },
+                },
+                {
+                    label: 'Open Recent',
+                    submenu: [
+                        ...(openRecent.length === 0
+                            ? [
+                                  {
+                                      label: 'No entries',
+                                      enabled: false,
+                                  } as MenuItemConstructorOptions,
+                              ]
+                            : openRecent.map<MenuItemConstructorOptions>((filepath, i) => ({
+                                  label: filepath,
+                                  accelerator: i <= 9 ? `CmdOrCtrl+${i + 1}` : undefined,
+                                  click: async () => {
+                                      mainWindow.webContents.send(
+                                          'file-open',
+                                          await openSaveFile(filepath)
+                                      );
+                                  },
+                              }))),
+                        { type: 'separator' },
+                        {
+                            label: 'Clear Recently Opened',
+                            click: () => {
+                                mainWindow.webContents.send('clear-open-recent');
+                            },
+                        },
+                    ],
+                },
+                { type: 'separator' },
+                {
+                    label: 'Save',
+                    accelerator: 'CmdOrCtrl+S',
+                    click: () => {
+                        mainWindow.webContents.send('file-save');
+                    },
+                },
+                {
+                    label: 'Save As...',
+                    accelerator: 'CmdOrCtrl+Shift+S',
+                    click: () => {
+                        mainWindow.webContents.send('file-save-as');
+                    },
+                },
+                { type: 'separator' },
+                isMac ? { role: 'close' } : { role: 'quit' },
+            ],
+        },
+        {
+            label: 'Edit',
+            submenu: [
+                {
+                    label: 'Undo',
+                    accelerator: 'CmdOrCtrl+Z',
+                    registerAccelerator: false,
+                    click: () => {
+                        mainWindow.webContents.send('history-undo');
+                    },
+                },
+                {
+                    label: 'Redo',
+                    accelerator: 'CmdOrCtrl+Y',
+                    registerAccelerator: false,
+                    click: () => {
+                        mainWindow.webContents.send('history-redo');
+                    },
+                },
+                { type: 'separator' },
+                {
+                    label: 'Cut',
+                    accelerator: 'CmdOrCtrl+X',
+                    registerAccelerator: false,
+                    click: () => {
+                        mainWindow.webContents.send('cut');
+                    },
+                },
+                {
+                    label: 'Copy',
+                    accelerator: 'CmdOrCtrl+C',
+                    registerAccelerator: false,
+                    click: () => {
+                        mainWindow.webContents.send('copy');
+                    },
+                },
+                {
+                    label: 'Paste',
+                    accelerator: 'CmdOrCtrl+V',
+                    registerAccelerator: false,
+                    click: () => {
+                        mainWindow.webContents.send('paste');
+                    },
+                },
+                // ...(isMac ? [
+                //   { role: 'delete' },
+                //   { role: 'selectAll' },
+                //   { type: 'separator' },
+                //   {
+                //     label: 'Speech',
+                //     submenu: [
+                //       { role: 'startSpeaking' },
+                //       { role: 'stopSpeaking' },
+                //     ],
+                //   },
+                // ] : [
+                //   { role: 'delete' },
+                //   { type: 'separator' },
+                //   { role: 'selectAll' },
+                // ]),
+            ],
+        },
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forceReload' },
+                { type: 'separator' },
+                { role: 'resetZoom' },
+                { role: 'zoomIn' },
+                { role: 'zoomOut' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' },
+            ],
+        },
+        {
+            label: 'Window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'zoom' },
+                ...(isMac
+                    ? [
+                          { type: 'separator' },
+                          { role: 'front' },
+                          { type: 'separator' },
+                          { role: 'window' },
+                      ]
+                    : [{ role: 'close' }]),
+                ...(!app.isPackaged ? [{ type: 'separator' }, { role: 'toggleDevTools' }] : []),
+            ],
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'View README',
+                    click: async () => {
+                        await shell.openExternal(
+                            'https://github.com/joeyballentine/chaiNNer/blob/main/README.md'
+                        );
+                    },
+                },
+                {
+                    label: 'Open logs folder',
+                    click: async () => {
+                        await shell.openPath(app.getPath('logs'));
+                    },
+                },
+                {
+                    label: 'Get ESRGAN models',
+                    click: async () => {
+                        await shell.openExternal('https://upscale.wiki/wiki/Model_Database');
+                    },
+                },
+                {
+                    label: 'Convert ONNX models to NCNN',
+                    click: async () => {
+                        await shell.openExternal('https://convertmodel.com/');
+                    },
+                },
+                { type: 'separator' },
+                {
+                    label: 'Collect system information',
+                    click: async () => {
+                        const [cpuInfo, gpuInfo] = await Promise.all([getCpuInfo(), getGpuInfo()]);
+
+                        const information: Record<string, unknown> = {
+                            app: {
+                                version: app.getVersion(),
+                                packaged: app.isPackaged,
+                                path: app.getAppPath(),
+                            },
+                            process: {
+                                cwd: process.cwd(),
+                                argv: process.argv,
+                            },
+                            os: {
+                                version: os.version(),
+                                release: os.release(),
+                                arch: os.arch(),
+                                endianness: os.endianness(),
+                            },
+                            cpu: { ...cpuInfo },
+                            gpus: gpuInfo.controllers.map((c) => ({ ...c })),
+                        };
+
+                        mainWindow.webContents.send('show-collected-information', information);
+                    },
+                },
+            ],
+        },
+    ] as MenuItemConstructorOptions[];
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+};
