54c06bbcb4e151c4809e157e711b0717bac547d9
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-06-08 16:47:40 +0200

    Remove dups and add more interval types

--- tests/common/types/data.ts
+++ tests/common/types/data.ts
@@ -19,14 +19,18 @@ import {
 const addExpressions = (expressions: readonly Expression[]): Expression[] => {
     const newExpressions: Expression[] = [...expressions];
 
-    for (const a of expressions) {
-        for (const b of expressions) {
+    for (let i = 0; i < expressions.length; i += 1) {
+        const a = expressions[i];
+        for (let j = i + 1; j < expressions.length; j += 1) {
+            const b = expressions[j];
             newExpressions.push(new UnionExpression([a, b]));
         }
     }
 
-    for (const a of expressions) {
-        for (const b of expressions) {
+    for (let i = 0; i < expressions.length; i += 1) {
+        const a = expressions[i];
+        for (let j = i + 1; j < expressions.length; j += 1) {
+            const b = expressions[j];
             newExpressions.push(new IntersectionExpression([a, b]));
         }
     }
@@ -52,6 +56,9 @@ export const types: readonly Type[] = [
     new NumericLiteralType(NaN),
     new IntervalType(0, 1),
     new IntervalType(0, 2),
+    new IntervalType(0.5, 1.5),
+    new IntervalType(0.5, 2.5),
+    new IntervalType(0.5, 0.75),
     new IntervalType(1, 2),
     new IntervalType(0, Infinity),
     new IntervalType(1, Infinity),
