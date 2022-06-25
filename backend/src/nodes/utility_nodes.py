470fba4ebc991f85a0fb9282a52236d7ebe6442a
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-06-26 01:14:16 +0200

    Added Text Pattern node (#477)
    * Added Text Pattern node
    
    * Fixes
    
    * Added description
    
    * Added test chain

--- backend/src/nodes/utility_nodes.py
+++ backend/src/nodes/utility_nodes.py
@@ -8,6 +8,7 @@ from typing import Union
 from .categories import UTILITY
 
 from .node_base import NodeBase
+from .utils.replacement import ReplacementString
 from .node_factory import NodeFactory
 from .properties.inputs import *
 from .properties.outputs import *
@@ -122,3 +123,38 @@ class TextAppendNode(NodeBase):
             str(x) for x in [str1, str2, str3, str4] if x != "" and x is not None
         ]
         return separator.join(strings)
+
+
+@NodeFactory.register("chainner:utility:text_pattern")
+class TextPatternNode(NodeBase):
+    def __init__(self):
+        super().__init__()
+        self.description = "Concatenate text using a pattern with a Python-like string interpolation syntax."
+        self.inputs = [
+            TextInput("Pattern", has_handle=False),
+            TextInput("{1}", allow_numbers=True).make_optional(),
+            TextInput("{2}", allow_numbers=True).make_optional(),
+            TextInput("{3}", allow_numbers=True).make_optional(),
+            TextInput("{4}", allow_numbers=True).make_optional(),
+        ]
+        self.outputs = [TextOutput("Output Text")]
+
+        self.category = UTILITY
+        self.name = "Text Pattern"
+        self.icon = "MdTextFields"
+        self.sub = "Text"
+
+    def run(
+        self,
+        pattern: str,
+        str1: Union[str, None],
+        str2: Union[str, None],
+        str3: Union[str, None],
+        str4: Union[str, None],
+    ) -> str:
+        replacements: dict[str, str] = {}
+        for i, s in enumerate([str1, str2, str3, str4]):
+            if s is not None:
+                replacements[str(i + 1)] = s
+
+        return ReplacementString(pattern).replace(replacements)
