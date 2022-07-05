2d6cc5641609fe10fd8ce18c60ad025c8d9573f5
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-07-05 16:12:48 +0200

    Added small type tag (#505)

--- src/renderer/components/outputs/GenericOutput.tsx
+++ src/renderer/components/outputs/GenericOutput.tsx
@@ -1,6 +1,9 @@
-import { Text } from '@chakra-ui/react';
+import { Flex, Spacer, Text } from '@chakra-ui/react';
 import { memo } from 'react';
+import { useContextSelector } from 'use-context-selector';
 import { Type } from '../../../common/types/types';
+import { GlobalVolatileContext } from '../../contexts/GlobalNodeState';
+import { TypeTag } from '../TypeTag';
 import OutputContainer from './OutputContainer';
 
 interface GenericOutputProps {
@@ -11,6 +14,10 @@ interface GenericOutputProps {
 }
 
 const GenericOutput = memo(({ label, id, outputId, definitionType }: GenericOutputProps) => {
+    const type = useContextSelector(GlobalVolatileContext, (c) =>
+        c.typeState.functions.get(id)?.outputs.get(outputId)
+    );
+
     return (
         <OutputContainer
             hasHandle
@@ -18,15 +25,19 @@ const GenericOutput = memo(({ label, id, outputId, definitionType }: GenericOutp
             id={id}
             outputId={outputId}
         >
-            <Text
-                marginInlineEnd="0.5rem"
-                mb={-1}
-                mt={-1}
-                textAlign="right"
-                w="full"
-            >
-                {label}
-            </Text>
+            <Flex w="full">
+                <Spacer />
+                {type && <TypeTag type={type} />}
+                <Text
+                    marginInlineEnd="0.5rem"
+                    mb={-1}
+                    ml={1}
+                    mt={-1}
+                    textAlign="right"
+                >
+                    {label}
+                </Text>
+            </Flex>
         </OutputContainer>
     );
 });
