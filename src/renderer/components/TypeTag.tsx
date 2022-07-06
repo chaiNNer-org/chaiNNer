525b3e50b9d825b94bef2336966dcde8e462bf56
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-07-07 00:20:10 +0200

    Added type tag for channels (#512)

--- src/renderer/components/TypeTag.tsx
+++ src/renderer/components/TypeTag.tsx
@@ -3,18 +3,38 @@ import { memo } from 'react';
 import { Type } from '../../common/types/types';
 import { isNumericLiteral } from '../../common/types/util';
 
-const getTypeText = (type: Type): string | undefined => {
-    if (isNumericLiteral(type)) return type.toString();
+const getColorMode = (channels: number) => {
+    switch (channels) {
+        case 1:
+            return 'Gray';
+        case 3:
+            return 'RGB';
+        case 4:
+            return 'RGBA';
+        default:
+            return undefined;
+    }
+};
 
+const getTypeText = (type: Type): string[] => {
+    if (isNumericLiteral(type)) return [type.toString()];
+
+    const tags: string[] = [];
     if (type.type === 'struct') {
         if (type.name === 'Image' && type.fields.length === 3) {
-            const [width, height /* , _channels */] = type.fields;
+            const [width, height, channels] = type.fields;
             if (isNumericLiteral(width.type) && isNumericLiteral(height.type)) {
-                return `${width.type.toString()}x${height.type.toString()}`;
+                tags.push(`${width.type.toString()}x${height.type.toString()}`);
+            }
+            if (isNumericLiteral(channels.type)) {
+                const mode = getColorMode(channels.type.value);
+                if (mode) {
+                    tags.push(mode);
+                }
             }
         }
     }
-    return undefined;
+    return tags;
 };
 
 export interface TypeTagProps {
@@ -22,20 +42,20 @@ export interface TypeTagProps {
 }
 
 export const TypeTag = memo(({ type }: TypeTagProps) => {
-    const text = getTypeText(type);
+    const tags = getTypeText(type);
 
     const tagColor = useColorModeValue('gray.400', 'gray.750');
     const tagFontColor = useColorModeValue('gray.700', 'gray.400');
 
     return (
-        // eslint-disable-next-line react/jsx-no-useless-fragment
         <>
-            {text && (
+            {tags.map((text) => (
                 <Tag
                     bgColor={tagColor}
                     color={tagFontColor}
                     fontSize="x-small"
                     height="15px"
+                    key={text}
                     lineHeight="auto"
                     minHeight="auto"
                     minWidth="auto"
@@ -47,7 +67,7 @@ export const TypeTag = memo(({ type }: TypeTagProps) => {
                 >
                     {text}
                 </Tag>
-            )}
+            ))}
         </>
     );
 });
