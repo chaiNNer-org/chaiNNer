108398d78ba4c0caa066984aef9a9623e0cccbc1
Joey Ballentine <joeyjballentine@gmail.com>
2022-07-23 18:16:14 -0400

    Sanitize process.env for spawned processes to remove external PYTHONHOME

--- src/common/env.ts
+++ src/common/env.ts
@@ -4,3 +4,7 @@ export const isMac = process.platform === 'darwin';
 export const isM1 = isMac && (os.cpus()[0]?.model.includes('Apple M1') ?? false);
 
 export const isRenderer = typeof process !== 'undefined' && process.type === 'renderer';
+
+const env = { ...process.env };
+delete env.PYTHONHOME;
+export const sanitizedEnv = env;
