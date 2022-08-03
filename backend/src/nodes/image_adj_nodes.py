294fa15898903e021486321469eb07039994b9f9
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-08-03 15:07:14 +0200

    Added Invert Color node (#688)

--- backend/src/nodes/image_adj_nodes.py
+++ backend/src/nodes/image_adj_nodes.py
@@ -338,3 +338,27 @@ class GammaNode(NodeBase):
         c = get_h_w_c(img)[2]
         img[:, :, : min(c, 3)] **= gamma
         return img
+
+
+@NodeFactory.register("chainner:image:invert")
+class InvertNode(NodeBase):
+    def __init__(self):
+        super().__init__()
+        self.description = "Inverts all colors in an image."
+        self.inputs = [ImageInput()]
+        self.outputs = [ImageOutput(image_type="Input0")]
+        self.category = IMAGE_ADJUSTMENT
+        self.name = "Invert Color"
+        self.icon = "MdInvertColors"
+        self.sub = "Adjustments"
+
+    def run(self, img: np.ndarray) -> np.ndarray:
+        c = get_h_w_c(img)[2]
+
+        # invert the first 3 channels
+        if c <= 3:
+            return 1 - img
+
+        img = img.copy()
+        img[:, :, :3] = 1 - img[:, :, :3]
+        return img
