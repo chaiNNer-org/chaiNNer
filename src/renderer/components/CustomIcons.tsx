16e8e757adae299797193b5fee0084a1f9c42493
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2022-08-10 13:32:14 -0400

    Refactored category information to come from backend (#715)
    * create category classes
    
    * Update frontend to use the new category class info
    
    * Use category classes instead of constants
    
    * Fix type hints
    
    * Rework how categories get put into nodes

--- src/renderer/components/CustomIcons.tsx
+++ src/renderer/components/CustomIcons.tsx
@@ -9,24 +9,6 @@ import * as md from 'react-icons/md';
 
 const libraries = { bs, cg, md, im };
 
-export const NumPyIcon = createIcon({
-    displayName: 'NumPyIcon',
-    viewBox: '0 0 28 29.93',
-    // path can also be an array of elements, if you have multiple paths, lines, shapes, etc.
-    path: (
-        <>
-            <path
-                d="M21 20.38V27l-5.88 2.93v-6.62Zm7-3.54v6.64L23 26v-6.63Zm-7.06-5.32v6.55L15.08 21v-6.57ZM28 8v6.51l-5 2.54V10.5Zm-13.81-.76 5.24 2.64L14 12.61 8.85 10ZM6.9 3.56l5 2.52-5.35 2.79-5.14-2.58Zm14.29 0 5.39 2.7-4.82 2.45-5.25-2.65ZM14 0l4.84 2.43-4.62 2.48-5-2.52Z"
-                fill="#00b5d8"
-            />
-            <path
-                d="m13 14.43-4-2V21S4.21 10.74 3.77 9.82c-.06-.12-.3-.25-.36-.29L0 7.79V23l3.52 1.88v-7.96l4.83 9.3a4.28 4.28 0 0 0 1.05 1.41c.68.45 3.61 2.22 3.61 2.22Z"
-                fill="#3182ce"
-            />
-        </>
-    ),
-});
-
 export const PyTorchIcon = createIcon({
     displayName: 'PyTorchIcon',
     viewBox: '0 0 20.97 24.99',
@@ -40,28 +22,6 @@ export const PyTorchIcon = createIcon({
             <path
                 d="M14.18 6.87a1.35 1.35 0 1 0-1.37-1.35 1.36 1.36 0 0 0 1.37 1.35Z"
                 fill="currentColor"
-            />
-        </>
-    ),
-});
-
-export const OpenCVIcon = createIcon({
-    displayName: 'OpenCVIcon',
-    viewBox: '0 0 25.98 23.88',
-    // path can also be an array of elements, if you have multiple paths, lines, shapes, etc.
-    path: (
-        <>
-            <path
-                d="M22.92 12.42a6.2 6.2 0 1 1-6.3 0l1.78 3a.16.16 0 0 1 0 .22 2.55 2.55 0 1 0 2.85 0 .16.16 0 0 1-.05-.22Z"
-                fill="#3182ce"
-            />
-            <path
-                d="M9.23 12.39a6.12 6.12 0 1 0 3.17 5.2H8.91a.16.16 0 0 0-.16.16 2.57 2.57 0 0 1-2.55 2.51 2.52 2.52 0 1 1 0-5 2.58 2.58 0 0 1 1 .21.16.16 0 0 0 .21-.06Z"
-                fill="#48bb78"
-            />
-            <path
-                d="M9.73 11.42a6.16 6.16 0 1 1 9.35-5.28 6.09 6.09 0 0 1-3 5.28l-1.77-3a.16.16 0 0 1 .05-.22 2.55 2.55 0 1 0-2.85 0 .17.17 0 0 1 0 .22Z"
-                fill="#c53030"
             />
         </>
     ),
@@ -127,19 +87,6 @@ export const NcnnIcon = createIcon({
     ),
 });
 
-export const IconFactoryOld = memo(({ category }: { category: string }) => {
-    switch (category) {
-        case 'NumPy':
-            return <NumPyIcon />;
-        case 'PyTorch':
-            return <PyTorchIcon />;
-        case 'OpenCV':
-            return <OpenCVIcon />;
-        default:
-            return null;
-    }
-});
-
 export const IconFactory = memo(
     ({
         icon,
@@ -150,95 +97,26 @@ export const IconFactory = memo(
         accentColor: string;
         boxSize?: number;
     }) => {
+        const unknownIcon = (
+            <Icon
+                alignContent="center"
+                alignItems="center"
+                as={bs.BsQuestionDiamond}
+                boxSize={boxSize}
+                color="gray.500"
+                transition="0.15s ease-in-out"
+            />
+        );
         if (!icon) {
-            return <OpenCVIcon />;
+            return unknownIcon;
         }
         switch (icon) {
-            case 'NumPy':
-                return (
-                    <NumPyIcon
-                        color={accentColor}
-                        transition="0.15s ease-in-out"
-                    />
-                );
+            // TODO: Get rid of these hardcoded icons
             case 'PyTorch':
                 return (
                     <PyTorchIcon
                         color={accentColor}
                         transition="0.15s ease-in-out"
-                    />
-                );
-            case 'Image':
-                // return <OpenCVIcon color={accentColor} transition="0.15s ease-in-out" />;
-                return (
-                    <Icon
-                        alignContent="center"
-                        alignItems="center"
-                        as={libraries.bs.BsImageFill}
-                        boxSize={boxSize}
-                        color={accentColor}
-                        transition="0.15s ease-in-out"
-                        viewBox="0 0 4 4"
-                    />
-                );
-            case 'Image (Dimensions)':
-                return (
-                    <Icon
-                        alignContent="center"
-                        alignItems="center"
-                        as={libraries.md.MdOutlinePhotoSizeSelectLarge}
-                        boxSize={boxSize}
-                        color={accentColor}
-                        transition="0.15s ease-in-out"
-                        viewBox="0 0 4 4"
-                    />
-                );
-            case 'Image (Adjustments)':
-                return (
-                    <Icon
-                        alignContent="center"
-                        alignItems="center"
-                        as={libraries.bs.BsSliders}
-                        boxSize={boxSize}
-                        color={accentColor}
-                        transition="0.15s ease-in-out"
-                        viewBox="0 0 4 4"
-                    />
-                );
-            case 'Image (Filters)':
-                return (
-                    <Icon
-                        alignContent="center"
-                        alignItems="center"
-                        as={libraries.md.MdFilterAlt}
-                        boxSize={boxSize}
-                        color={accentColor}
-                        transition="0.15s ease-in-out"
-                        viewBox="0 0 4 4"
-                    />
-                );
-            case 'Image (Utilities)':
-                return (
-                    <Icon
-                        alignContent="center"
-                        alignItems="center"
-                        as={libraries.bs.BsGear}
-                        boxSize={boxSize}
-                        color={accentColor}
-                        transition="0.15s ease-in-out"
-                        viewBox="0 0 4 4"
-                    />
-                );
-            case 'Image (Channels)':
-                return (
-                    <Icon
-                        alignContent="center"
-                        alignItems="center"
-                        as={libraries.md.MdAllOut}
-                        boxSize={boxSize}
-                        color={accentColor}
-                        transition="0.15s ease-in-out"
-                        viewBox="0 0 4 4"
                     />
                 );
             case 'ONNX':
@@ -255,18 +133,6 @@ export const IconFactory = memo(
                         transition="0.15s ease-in-out"
                     />
                 );
-            case 'Utility':
-                return (
-                    <Icon
-                        alignContent="center"
-                        alignItems="center"
-                        as={libraries.bs.BsGearFill}
-                        boxSize={boxSize}
-                        color={accentColor}
-                        transition="0.15s ease-in-out"
-                        viewBox="0 0 4 4"
-                    />
-                );
             default:
             // nothing
         }
@@ -276,7 +142,7 @@ export const IconFactory = memo(
             prefix
         ];
         if (!library) {
-            return <OpenCVIcon />;
+            return unknownIcon;
         }
         const libraryIcon = library[icon];
         return (
