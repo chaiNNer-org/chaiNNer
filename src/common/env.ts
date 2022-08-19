702b3edeb09a3aef83d9e60fb516d5b8a1ad68ed
Joey Ballentine <joeyjballentine@gmail.com>
2022-08-18 23:00:24 -0400

    Fix PYTHONHOME variable deletion causing crash

--- src/common/env.ts
+++ src/common/env.ts
@@ -6,5 +6,7 @@ export const isM1 = isMac && (os.cpus()[0]?.model.includes('Apple M1') ?? false)
 export const isRenderer = typeof process !== 'undefined' && process.type === 'renderer';
 
 const env = { ...process.env };
-delete env.PYTHONHOME;
+if (env.PYTHONHOME) {
+    delete env.PYTHONHOME;
+}
 export const sanitizedEnv = env;
