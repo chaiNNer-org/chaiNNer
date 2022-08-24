536d23cf9c55f830c1b6b6c9e5045b03dd15edbe
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2022-08-24 17:11:47 -0400

    SwinIR Support (#812)
    * add swinir, initial
    
    * kinda working
    
    * Fix "classical_sr" swinir models not working with fp16
    
    * Backend linting fixes
    
    * Add links to supported arches in README (#817)
    
    * update docs

--- backend/src/nodes/utils/torch_types.py
+++ backend/src/nodes/utils/torch_types.py
@@ -3,5 +3,6 @@ from .architecture.RRDB import RRDBNet as ESRGAN
 from .architecture.SPSR import SPSRNet as SPSR
 from .architecture.SRVGG import SRVGGNetCompact as RealESRGANv2
 from .architecture.SwiftSRGAN import Generator as SwiftSRGAN
+from .architecture.SwinIR import SwinIR
 
-PyTorchModel = Union[RealESRGANv2, SPSR, SwiftSRGAN, ESRGAN]
+PyTorchModel = Union[RealESRGANv2, SPSR, SwiftSRGAN, ESRGAN, SwinIR]
