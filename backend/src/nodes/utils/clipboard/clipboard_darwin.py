c245fa5d046a522cfeb72ca671d2c75503b52779
Sryvkver <git@xn--lea.dev>
2022-09-08 17:11:51 +0200

    Fixed Mac copy to clipboard

--- backend/src/nodes/utils/clipboard/clipboard_darwin.py
+++ backend/src/nodes/utils/clipboard/clipboard_darwin.py
@@ -1,23 +1,11 @@
 import numpy as np
-import shutil
-import subprocess
+import pasteboard
 
 from .clipboard_base import ClipboardBase
 
 class DarwinClipboard(ClipboardBase):
     def __init__(self) -> None:
-        self.pbcopy = shutil.which('pbcopy')
-        if not self.pbcopy:
-            raise Exception("pbcopy not found. pbcopy must be installed and available on PATH")
+        self.pb = pasteboard.Pasteboard()
 
     def copy_image(self, img: bytes) -> None:
-        if self.pbcopy is None:
-            raise Exception("pbcopy not found. pbcopy must be installed and available on PATH")
-
-        process_args = [self.pbcopy]
-        proc = subprocess.Popen(process_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = proc.communicate(img)
-        if proc.returncode != 0:
-            raise Exception(f"Copy failed. pbcopy returned code: {proc.returncode!r} "
-                                     f"Stderr: {stderr!r} "
-                                     f"Stdout: {stdout!r}")
+        self.pb.set_contents(img, pasteboard.PNG)
