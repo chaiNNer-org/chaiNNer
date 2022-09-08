1bb9d8b5d8f1f73ed42e92ad98fe52e335f8535a
Sryvkver <git@xn--lea.dev>
2022-09-08 16:01:57 +0200

    Added macos Clipboard

--- backend/src/nodes/utils/clipboard/clipboard_darwin.py
+++ backend/src/nodes/utils/clipboard/clipboard_darwin.py
@@ -6,7 +6,18 @@ from .clipboard_base import ClipboardBase
 
 class DarwinClipboard(ClipboardBase):
     def __init__(self) -> None:
-        pass
+        self.pbcopy = shutil.which('pbcopy')
+        if not self.pbcopy:
+            raise Exception("pbcopy not found. pbcopy must be installed and available on PATH")
 
     def copy_image(self, img: bytes) -> None:
-        pass
+        if self.pbcopy is None:
+            raise Exception("pbcopy not found. pbcopy must be installed and available on PATH")
+
+        process_args = [self.pbcopy]
+        proc = subprocess.Popen(process_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = proc.communicate(img)
+        if proc.returncode != 0:
+            raise Exception(f"Copy failed. pbcopy returned code: {proc.returncode!r} "
+                                     f"Stderr: {stderr!r} "
+                                     f"Stdout: {stdout!r}")
