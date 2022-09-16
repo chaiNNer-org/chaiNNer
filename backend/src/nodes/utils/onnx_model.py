21f0f1a9f967577bcf66e523c17e9f7b45e3b241
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-09-16 22:20:54 +0200

    Added a class for ONNX models

---
+++ backend/src/nodes/utils/onnx_model.py
@@ -0,0 +1,5 @@
+# This class defines an interface.
+# It is important that is does not contain types that depend on ONNX.
+class OnnxModel:
+    def __init__(self, model_as_bytes: bytes):
+        self.bytes = model_as_bytes
---
+++ backend/src/nodes/utils/onnx_session.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+import onnxruntime as ort
+from weakref import WeakKeyDictionary
+
+from .exec_options import ExecutionOptions
+from .onnx_model import OnnxModel
+
+
+def create_inference_session(
+    model: OnnxModel, exec_options: ExecutionOptions
+) -> ort.InferenceSession:
+    if exec_options.onnx_execution_provider == "TensorrtExecutionProvider":
+        providers = [
+            (
+                "TensorrtExecutionProvider",
+                {
+                    "device_id": exec_options.onnx_gpu_index,
+                },
+            ),
+            (
+                "CUDAExecutionProvider",
+                {
+                    "device_id": exec_options.onnx_gpu_index,
+                },
+            ),
+            "CPUExecutionProvider",
+        ]
+    elif exec_options.onnx_execution_provider == "CUDAExecutionProvider":
+        providers = [
+            (
+                "CUDAExecutionProvider",
+                {
+                    "device_id": exec_options.onnx_gpu_index,
+                },
+            ),
+            "CPUExecutionProvider",
+        ]
+    else:
+        providers = [exec_options.onnx_execution_provider, "CPUExecutionProvider"]
+
+    session = ort.InferenceSession(model.bytes, providers=providers)
+    return session
+
+
+__session_cache: WeakKeyDictionary[
+    OnnxModel, ort.InferenceSession
+] = WeakKeyDictionary()
+
+
+def get_onnx_session(
+    model: OnnxModel, exec_options: ExecutionOptions
+) -> ort.InferenceSession:
+    cached = __session_cache.get(model)
+    if cached is None:
+        cached = create_inference_session(model, exec_options)
+        __session_cache[model] = cached
+    return cached
