ec84f4273a04ca2e5c6797e1e282e4ba5cc27308
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-09-16 15:03:17 +0200

    Fixed starting nodes not running after clearing (#974)
    * Fixed starting nodes not sunning after clearing
    
    * Renamed `lastRunInputHash` -> `lastInputHash`

--- src/renderer/hooks/useRunNode.ts
+++ src/renderer/hooks/useRunNode.ts
@@ -30,15 +30,19 @@ export const useRunNode = ({ inputData, id, schemaId }: NodeData, shouldRun: boo
         [inputData]
     );
     const inputHash = useMemo(() => JSON.stringify(inputs), [inputData]);
-    const lastRunInputHash = useRef<string>();
+    const lastInputHash = useRef<string>();
     useAsyncEffect(
         async (token) => {
-            if (shouldRun && inputHash !== lastRunInputHash.current) {
-                // give it some time for other effects to settle in
-                await delay(50);
-                token.checkCanceled();
+            if (inputHash === lastInputHash.current) {
+                return;
+            }
+            // give it some time for other effects to settle in
+            await delay(50);
+            token.checkCanceled();
 
-                lastRunInputHash.current = inputHash;
+            lastInputHash.current = inputHash;
+
+            if (shouldRun) {
                 animate([id], false);
 
                 const result = await backend.runIndividual({
