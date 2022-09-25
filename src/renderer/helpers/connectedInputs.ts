215ba18d8bb2be14b7050d9e85553304fea7b81d
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-09-25 17:10:11 +0200

    Moved checkNodeValidity into common (#1032)

---
+++ src/renderer/helpers/connectedInputs.ts
@@ -0,0 +1,13 @@
+import { Edge } from 'react-flow-renderer';
+import { EdgeData, InputId } from '../../common/common-types';
+import { parseTargetHandle } from '../../common/util';
+
+export const getConnectedInputs = (
+    nodeId: string,
+    edges: readonly Edge<EdgeData>[]
+): Set<InputId> => {
+    const targetedInputs = edges
+        .filter((e) => e.target === nodeId && e.targetHandle)
+        .map((e) => parseTargetHandle(e.targetHandle!).inOutId);
+    return new Set(targetedInputs);
+};
