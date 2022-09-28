Fix setting the variables for precision changes (#1040)
Fix setting the variables for precision changes.

backend/src/nodes/pytorch_nodes.py
@@ -87,9 +87,9 @@ class LoadModelNode(NodeBase):
             model = model.to(torch.device(exec_options.device))
             should_use_fp16 = exec_options.fp16 and model.supports_fp16
             if should_use_fp16:
-                model.half()
+                model = model.half()
             else:
-                model.float()
+                model = model.float()
         except ValueError as e:
             raise e
         except Exception:
@@ -462,11 +462,11 @@ class ConvertTorchToONNXNode(NodeBase):
 
         should_use_fp16 = exec_options.fp16 and model.supports_fp16
         if should_use_fp16:
-            model.half()
-            dummy_input.half()
+            model = model.half()
+            dummy_input = dummy_input.half()
         else:
-            model.float()
-            dummy_input.float()
+            model = model.float()
+            dummy_input = dummy_input.float()
 
         with BytesIO() as f:
             torch.onnx.export(