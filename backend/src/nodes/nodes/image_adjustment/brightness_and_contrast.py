3a7c8af067ca8f9646610fb4db710179d27f3aee
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-10-10 23:00:31 +0200

    Brightness changes (#1081)
    * New B&C implementation
    
    * Added migration
    
    * Removed unused import

--- backend/src/nodes/nodes/image_adjustment/brightness_and_contrast.py
+++ backend/src/nodes/nodes/image_adjustment/brightness_and_contrast.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import cv2
 import numpy as np
 
 from . import category as ImageAdjustmentCategory
@@ -8,6 +7,7 @@ from ...node_base import NodeBase
 from ...node_factory import NodeFactory
 from ...properties.inputs import ImageInput, SliderInput
 from ...properties.outputs import ImageOutput
+from ...utils.utils import get_h_w_c
 
 
 @NodeFactory.register("chainner:image:brightness_and_contrast")
@@ -40,42 +40,31 @@ class BrightnessAndContrastNode(NodeBase):
         self.icon = "ImBrightnessContrast"
         self.sub = "Adjustments"
 
-    def run(self, img: np.ndarray, b_amount: float, c_amount: float) -> np.ndarray:
+    def run(self, img: np.ndarray, brightness: float, contrast: float) -> np.ndarray:
         """Adjusts the brightness and contrast of an image"""
 
-        b_norm_amount = b_amount / 100
-        c_norm_amount = c_amount / 100
+        brightness /= 100
+        contrast /= 100
 
-        # Pass through unadjusted image
-        if b_norm_amount == 0 and c_norm_amount == 0:
+        if brightness == 0 and contrast == 0:
             return img
 
-        # Calculate brightness adjustment
-        if b_norm_amount > 0:
-            shadow = b_norm_amount
-            highlight = 1
+        _, _, c = get_h_w_c(img)
+
+        # Contrast correction factor
+        max_c = 259 / 255
+        factor: float = (max_c * (contrast + 1)) / (max_c - contrast)
+        add: float = factor * brightness + 0.5 * (1 - factor)
+
+        if c <= 3:
+            img = factor * img + add
         else:
-            shadow = 0
-            highlight = 1 + b_norm_amount
-        alpha_b = highlight - shadow
-        if img.ndim == 2:
-            img = cv2.addWeighted(img, alpha_b, img, 0, shadow)
-        else:
-            img[:, :, :3] = cv2.addWeighted(
-                img[:, :, :3], alpha_b, img[:, :, :3], 0, shadow
+            img = np.concatenate(
+                [
+                    factor * img[:, :, :3] + add,
+                    img[:, :, 3:],
+                ],
+                axis=2,
             )
 
-        # Calculate contrast adjustment
-        alpha_c = ((259 / 255) * (c_norm_amount + 1)) / (
-            (259 / 255) - c_norm_amount
-        )  # Contrast correction factor
-        gamma_c = 0.5 * (1 - alpha_c)
-        if img.ndim == 2:
-            img = cv2.addWeighted(img, alpha_c, img, 0, gamma_c)
-        else:
-            img[:, :, :3] = cv2.addWeighted(
-                img[:, :, :3], alpha_c, img[:, :, :3], 0, gamma_c
-            )
-        img = np.clip(img, 0, 1).astype("float32")
-
-        return img
+        return np.clip(img, 0, 1)
