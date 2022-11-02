26fad56cc903f673b7d5c83ffb587c8d8b7121c7
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2022-11-02 12:55:27 -0400

    Add support for Swin2SR (#1158)
    * Add support for Swin2SR
    
    * remove log
    
    * Add documentation
    
    * pylint skip file

--- backend/src/nodes/utils/torch_types.py
+++ backend/src/nodes/utils/torch_types.py
@@ -4,9 +4,17 @@ from .architecture.SPSR import SPSRNet as SPSR
 from .architecture.SRVGG import SRVGGNetCompact as RealESRGANv2
 from .architecture.SwiftSRGAN import Generator as SwiftSRGAN
 from .architecture.SwinIR import SwinIR
+from .architecture.Swin2SR import Swin2SR
 from .architecture.GFPGAN.gfpganv1_clean_arch import GFPGANv1Clean
 from .architecture.GFPGAN.restoreformer_arch import RestoreFormer
 
 PyTorchModel = Union[
-    RealESRGANv2, SPSR, SwiftSRGAN, ESRGAN, SwinIR, GFPGANv1Clean, RestoreFormer
+    RealESRGANv2,
+    SPSR,
+    SwiftSRGAN,
+    ESRGAN,
+    SwinIR,
+    Swin2SR,
+    GFPGANv1Clean,
+    RestoreFormer,
 ]
