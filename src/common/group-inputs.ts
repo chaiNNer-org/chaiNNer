aa042c7f579b2b4b8a89d2be8f4afcad8bc24b84
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-11-08 21:11:55 +0100

    Added conditional enum inputs (#1200)

--- src/common/group-inputs.ts
+++ src/common/group-inputs.ts
@@ -4,6 +4,7 @@ import { DropDownInput, FileInput, Group, GroupKind, Input } from './common-type
 type InputGuarantees<T extends Record<GroupKind, readonly Input[]>> = T;
 
 type DeclaredGroupInputs = InputGuarantees<{
+    'conditional-enum': readonly [DropDownInput, ...Input[]];
     'from-to-dropdowns': readonly [DropDownInput, DropDownInput];
     'ncnn-file-inputs': readonly [FileInput, FileInput];
     'optional-list': readonly [Input, ...Input[]];
@@ -20,6 +21,20 @@ export const groupInputsChecks: {
         group: Group & { readonly type: Type }
     ) => boolean;
 } = {
+    'conditional-enum': (inputs, { options: { conditions } }) => {
+        if (inputs.length === 0) return false;
+
+        const dropdown = inputs[0];
+        if (dropdown.kind !== 'dropdown') return false;
+        if (dropdown.hasHandle) return false;
+        const allowed = new Set(dropdown.options.map((o) => o.value));
+        const idStrings = new Set(inputs.slice(1).map((i) => String(i.id)));
+
+        return Object.entries(conditions).every(([id, cond]) => {
+            if (!cond) return true;
+            return idStrings.has(id) && cond.length > 0 && cond.every((c) => allowed.has(c));
+        });
+    },
     'from-to-dropdowns': (inputs) => {
         return (
             inputs.length === 2 &&
