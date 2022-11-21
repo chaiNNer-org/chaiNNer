c7801557298ebf1a44a83a7e21104dd7b69de4ed
Joey Ballentine <joeyjballentine@gmail.com>
2022-11-21 00:17:54 -0500

    Use restricted unpickler

---
+++ backend/src/nodes/utils/unpickler.py
@@ -0,0 +1,26 @@
+# Safe unpickler to prevent arbitrary code execution
+import pickle
+from types import SimpleNamespace
+
+safe_list = {
+    ("collections", "OrderedDict"),
+    ("torch._utils", "_rebuild_tensor_v2"),
+    ("torch", "FloatStorage"),
+}
+
+
+class RestrictedUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        # Only allow required classes to load state dict
+        if (module, name) not in safe_list:
+            raise pickle.UnpicklingError(
+                "Global '{}.{}' is forbidden".format(module, name)
+            )
+        return super().find_class(module, name)
+
+
+RestrictedUnpickle = SimpleNamespace(
+    Unpickler=RestrictedUnpickler,
+    __name__="pickle",
+    load=lambda *args, **kwargs: RestrictedUnpickler(*args, **kwargs).load(),
+)
