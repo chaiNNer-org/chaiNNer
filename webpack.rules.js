71953cac53e2a47655b6bb6df8aa5c580e5e5566
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-11-24 01:14:36 +0100

    Fixed webpack rules

--- webpack.rules.js
+++ webpack.rules.js
@@ -20,12 +20,14 @@ module.exports = [
                     ],
                     ...(isDevelopment
                         ? [
-                              'i18next-extract',
-                              {
-                                  outputPath: './src/common/locales/{{locale}}/{{ns}}.json',
-                                  keyAsDefaultValueForDerivedKeys: true,
-                                  discardOldKeys: true,
-                              },
+                              [
+                                  'i18next-extract',
+                                  {
+                                      outputPath: './src/common/locales/{{locale}}/{{ns}}.json',
+                                      keyAsDefaultValueForDerivedKeys: true,
+                                      discardOldKeys: true,
+                                  },
+                              ],
                           ]
                         : []),
                 ],
