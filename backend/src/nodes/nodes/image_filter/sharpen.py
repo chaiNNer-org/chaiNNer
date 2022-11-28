e097cffe161f508e0b431570216278b127c3e453
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-11-28 18:40:31 +0100

    Added threshold to Unsharp Mask (#1294)

--- backend/src/nodes/nodes/image_filter/sharpen.py
+++ backend/src/nodes/nodes/image_filter/sharpen.py
@@ -27,6 +27,15 @@ class SharpenNode(NodeBase):
                 controls_step=1,
                 scale="log",
             ),
+            SliderInput(
+                "Threshold",
+                minimum=0,
+                maximum=100,
+                default=0,
+                precision=1,
+                controls_step=1,
+                scale="log",
+            ),
         ]
         self.outputs = [ImageOutput(image_type="Input0")]
         self.category = ImageFilterCategory
@@ -39,11 +48,19 @@ class SharpenNode(NodeBase):
         img: np.ndarray,
         radius: float,
         amount: float,
+        threshold: float,
     ) -> np.ndarray:
         if radius == 0 or amount == 0:
             return img
 
         blurred = cv2.GaussianBlur(img, (0, 0), radius)
-        img = cv2.addWeighted(img, amount + 1, blurred, -amount, 0)
+
+        threshold /= 100
+        if threshold == 0:
+            img = cv2.addWeighted(img, amount + 1, blurred, -amount, 0)
+        else:
+            diff = img - blurred
+            diff = np.sign(diff) * np.maximum(0, np.abs(diff) - threshold)
+            img += diff * amount
 
         return np.clip(img, 0, 1)
