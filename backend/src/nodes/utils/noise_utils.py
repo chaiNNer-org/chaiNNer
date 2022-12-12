60fea797afec0796d2480f7eb2f5cbbd43146f65
theflyingzamboni <theaerialzamboni@gmail.com>
2022-12-12 18:06:50 -0500

    Adjusted dtypes of noise arrays to work with cvtColor

--- backend/src/nodes/utils/noise_utils.py
+++ backend/src/nodes/utils/noise_utils.py
@@ -4,6 +4,8 @@ import numpy as np
 from .utils import get_h_w_c
 from .image_utils import as_target_channels
 
+from sanic.log import logger
+
 
 def __add_noises(
     image: np.ndarray,
@@ -18,6 +20,7 @@ def __add_noises(
         img = img[:, :, :3]
 
     noises = noise_gen(h, w)
+    logger.info(noises[0].dtype)
     assert len(noises) > 0
 
     max_channels = min(c, 3)
@@ -63,7 +66,7 @@ def gaussian_noise(
     noise_c = 3 if noise_type == "rgb" else 1
     return __add_noise(
         image,
-        lambda h, w: rng.normal(0, amount, (h, w, noise_c)),
+        lambda h, w: rng.normal(0, amount, (h, w, noise_c)).astype(np.float32),
     )
 
 
@@ -78,7 +81,7 @@ def uniform_noise(
     noise_c = 3 if noise_type == "rgb" else 1
     return __add_noise(
         image,
-        lambda h, w: rng.uniform(-amount, amount, (h, w, noise_c)),
+        lambda h, w: rng.uniform(-amount, amount, (h, w, noise_c)).astype(np.float32),
     )
 
 
@@ -93,8 +96,8 @@ def salt_and_pepper_noise(
         rng = np.random.default_rng(seed)
         noise_c = 3 if noise_type == "rgb" else 1
         amt = amount / 2
-        pepper = rng.choice([0, 1], (h, w, noise_c), p=[amt, 1 - amt])
-        salt = rng.choice([0, 1], (h, w, noise_c), p=[1 - amt, amt])
+        pepper = rng.choice([0, 1], (h, w, noise_c), p=[amt, 1 - amt]).astype(np.uint8)
+        salt = rng.choice([0, 1], (h, w, noise_c), p=[1 - amt, amt]).astype(np.uint8)
         return [pepper, salt]
 
     def combine(i: np.ndarray, n: List[np.ndarray]):
@@ -115,7 +118,7 @@ def poisson_noise(
     noise_c = 3 if noise_type == "rgb" else 1
     return __add_noise(
         image,
-        lambda h, w: rng.poisson(amount, (h, w, noise_c)),
+        lambda h, w: rng.poisson(amount, (h, w, noise_c)).astype(np.uint8),
     )
 
 
@@ -130,6 +133,6 @@ def speckle_noise(
     noise_c = 3 if noise_type == "rgb" else 1
     return __add_noise(
         image,
-        lambda h, w: rng.normal(0, amount, (h, w, noise_c)),
+        lambda h, w: rng.normal(0, amount, (h, w, noise_c)).astype(np.float32),
         lambda i, n: i + i * n,
     )
