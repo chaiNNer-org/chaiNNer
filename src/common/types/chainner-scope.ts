60645d876397e899585d9d3b31812f11b083f5f4
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-12-19 20:16:15 +0100

    Make BlendMode an enum

--- src/common/types/chainner-scope.ts
+++ src/common/types/chainner-scope.ts
@@ -74,7 +74,6 @@ struct IteratorAuto;
 // various inputs
 struct AdaptiveMethod;
 struct AdaptiveThresholdType;
-struct BlendMode;
 struct ColorSpace { channels: 1 | 3 | 4, supportsAlpha: bool }
 struct DdsFormat;
 struct DdsMipMaps;
