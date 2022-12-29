590b81690ec5d97f5a2e1e8a3e60cbcb2369f147
JumpyJacko <bobjacklee27@gmail.com>
2022-12-29 15:48:58 +1100

    Removed redundant code comments

--- backend/src/nodes/nodes/image_utility/convolve.py
+++ backend/src/nodes/nodes/image_utility/convolve.py
@@ -47,21 +47,17 @@ class ImageConvolveNode(NodeBase):
             img = img[:, :, 0]
 
         # Thanks Samrat Sahoo on Medium for the convolution code
-        # Cross Correlation
         kernel = np.flipud(np.fliplr(kernel))
 
-        # Gather Shapes of Kernel + Image + Padding
         xKernShape = kernel.shape[0]
         yKernShape = kernel.shape[1]
         xImgShape = img.shape[0]
         yImgShape = img.shape[1]
 
-        # Shape of Output Convolution
         xOutput = int(((xImgShape - xKernShape + 2 * padding) / strides) + 1)
         yOutput = int(((yImgShape - yKernShape + 2 * padding) / strides) + 1)
         output = np.zeros((xOutput, yOutput))
 
-        # Apply Equal Padding to All Sides
         if padding != 0:
             imagePadded = np.zeros(
                 (img.shape[0] + padding * 2, img.shape[1] + padding * 2)
@@ -72,19 +68,14 @@ class ImageConvolveNode(NodeBase):
         else:
             imagePadded = img
 
-        # Iterate through image
         for y in range(img.shape[1]):
-            # Exit Convolution
             if y > img.shape[1] - yKernShape:
                 break
-            # Only Convolve if y has gone down by the specified Strides
             if y % strides == 0:
                 for x in range(img.shape[0]):
-                    # Go to next row once kernel is out of bounds
                     if x > img.shape[0] - xKernShape:
                         break
                     try:
-                        # Only Convolve if x has moved by the specified Strides
                         if x % strides == 0:
                             output[x, y] = (
                                 kernel
