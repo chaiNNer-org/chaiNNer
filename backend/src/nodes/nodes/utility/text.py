c8839d3037131ef96c2fd2e406c7d53c6cd2aab0
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-01-07 01:26:00 +0100

    Added Number and Text nodes

---
+++ backend/src/nodes/nodes/utility/text.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from . import category as UtilityCategory
+
+from ...node_base import NodeBase
+from ...node_factory import NodeFactory
+from ...properties.inputs import TextInput
+from ...properties.outputs import TextOutput
+
+
+@NodeFactory.register("chainner:utility:text")
+class TextAppendNode(NodeBase):
+    def __init__(self):
+        super().__init__()
+        self.description = "Outputs the given text."
+        self.inputs = [
+            TextInput("Text", min_length=0),
+        ]
+        self.outputs = [
+            TextOutput("Text", output_type="toString(Input0)"),
+        ]
+
+        self.category = UtilityCategory
+        self.name = "Text"
+        self.icon = "MdTextFields"
+        self.sub = "Value"
+
+    def run(self, text: str) -> str:
+        return text
