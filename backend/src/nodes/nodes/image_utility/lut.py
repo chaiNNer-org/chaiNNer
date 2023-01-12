a302f21b23ce31d034e846d1c835c427ddc12581
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-01-12 17:28:24 +0100

    Quantize to the size of the LUT

--- backend/src/nodes/nodes/image_utility/lut.py
+++ backend/src/nodes/nodes/image_utility/lut.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 import numpy as np
-import cv2
 
 from . import category as ImageUtilityCategory
 from ...node_base import NodeBase
@@ -9,6 +8,27 @@ from ...properties.inputs import ImageInput
 from ...properties.outputs import ImageOutput
 from ...properties import expression
 from ...utils.utils import get_h_w_c
+
+
+def quantize(img: np.ndarray, levels: int) -> np.ndarray:
+    """
+    Given an array for float32 values between 0 and 1, this will return a quantized version of the array with the given number of quantization levels.
+
+    The type of the integers in the returned array will be the smallest unsigned integer type that can fit `levels` many values. E.g. uint8 is used for 256, and uint16 is used for 257 levels.
+    """
+    assert levels >= 1
+    assert (
+        levels <= 2**24
+    ), "Quantizing float32 values with more than 2**24 levels doesn't make sense, because only integers up to 2**24 can be represented exactly using float32."
+
+    q: np.ndarray = np.round(img * (levels - 1))
+
+    if levels <= 256:
+        return q.astype(np.uint8)
+    elif levels <= 65536:
+        return q.astype(np.uint16)
+    else:
+        return q.astype(np.uint32)
 
 
 @NodeFactory.register("chainner:image:lut")
@@ -40,13 +60,9 @@ class LutNode(NodeBase):
     ) -> np.ndarray:
         # only use top row
         lut = lut[0:1]
-
         _, w, _ = get_h_w_c(lut)
-        if w != 256:
-            # this intentionally uses OpenCV for resizing because it resizes all channels independently of each other.
-            lut = cv2.resize(lut, (256, 1), interpolation=cv2.INTER_LINEAR)
 
-        # convert to 8bit
-        img = np.round(img * 255).astype(np.uint8)
+        # convert to the size of the LUT
+        img = quantize(img, w)
 
         return lut[0, img]
