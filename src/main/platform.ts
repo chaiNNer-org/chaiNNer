3d274acefd97e164686d23dd880b4d34c468593f
Joey Ballentine <joeyjballentine@gmail.com>
2023-01-15 15:22:25 -0500

    remove unused promise based funcs

--- src/main/platform.ts
+++ src/main/platform.ts
@@ -2,7 +2,7 @@ import { app } from 'electron';
 import { existsSync } from 'fs';
 import os from 'os';
 import path from 'path';
-import { checkFileExists, lazy } from '../common/util';
+import { lazy } from '../common/util';
 
 export type SupportedPlatform = 'linux' | 'darwin' | 'win32';
 
@@ -21,17 +21,6 @@ export const getPlatform = (): SupportedPlatform => {
 };
 
 export const currentExecutableDir = path.dirname(app.getPath('exe'));
-
-export const getIsPortable = async (): Promise<boolean> => {
-    const isPortable = await checkFileExists(path.join(currentExecutableDir, 'portable'));
-    return isPortable;
-};
-
-export const getRootDir = async (): Promise<string> => {
-    const isPortable = await getIsPortable();
-    const rootDir = isPortable ? currentExecutableDir : app.getPath('userData');
-    return rootDir;
-};
 
 export const getIsPortableSync = lazy((): boolean => {
     const isPortable = existsSync(path.join(currentExecutableDir, 'portable'));
