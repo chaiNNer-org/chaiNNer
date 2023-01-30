6b113673f97ad2189df02548fdd4a6eb0ea94f6b
Joey Ballentine <joeyjballentine@gmail.com>
2023-01-29 21:01:55 -0500

    Force a backend restart when killing the backend takes over a second

--- src/renderer/contexts/ExecutionContext.tsx
+++ src/renderer/contexts/ExecutionContext.tsx
@@ -328,8 +328,21 @@ export const ExecutionProvider = memo(({ children }: React.PropsWithChildren<{}>
 
     const kill = useCallback(async () => {
         try {
-            const response = await backend.kill();
-            if (response.type === 'error') {
+            // Try to kill the current executor
+            // If it doesn't respond within 1 second, force restart it
+            const backendKillPromise = backend.kill();
+            const timeoutPromise = new Promise<string>((resolve) => {
+                setTimeout(() => {
+                    resolve('timeout');
+                }, 1000);
+            });
+            const response = await Promise.race([backendKillPromise, timeoutPromise]);
+            if (response === 'timeout') {
+                // Force restart the backend
+                await ipcRenderer.invoke('restart-backend');
+                return;
+            }
+            if (typeof response !== 'string' && response.type === 'error') {
                 sendAlert({ type: AlertType.ERROR, message: response.exception });
             }
         } catch (err) {
