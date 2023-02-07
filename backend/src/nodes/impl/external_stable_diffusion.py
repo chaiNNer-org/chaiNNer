fc9fa32003f695cfdbf9dc05e79f63a5e701e06c
Alex Dodge <alexdodge@gmail.com>
2023-02-07 10:33:10 -0800

    Stable Diffusion via REST (#1500)
    * Async node base class
    
    - Exposes a run_async() coroutine
    - processed appropriately in process.py
    
    * helper functions for REST
    
    - globals for interacting with the automatic1111 API
    - encoding/decoding images from the JSON
    - async HTTP call to the API
    
    * REST category
    
    - Nodes interacting with the automatic1111 API
    - text2img, img2img, and interrogate
    
    * ...
    
    * ...
    
    * ...
    
    * ...
    
    * request options from the API on import
    
    - causes the nodes to be hidden if the API isn't up
    - also these options might be useful later
    
    * icons
    
    * lint and black
    
    * Responding to feedback
    
    - renaming REST to External Stable Diffusion
    - making some text arguments optional
    - using sliders where appropriate
    - using the "seed" group
    - geting rid of async
        - we're already calling the nodes in a thread, so we don't need individual http calls to be async
    - replace aiohttp with requests in requirements
    
    * Change subcategory to "Automatic1111"
    
    * ...
    
    * formatting
    
    * Responding to feedback
    
    - Added requests library to requirements.ts
    - Removed kebab case in favor of "Text to Image"
    - Changed LargeImageOutput to ImageOutput
    
    * Add type hints to node output.
    
    * ...
    
    * Check size of returned image.
    
    - Actually looks like SD rounds down to the nearest multiple of 8
    - Added slider_step and control_step to SliderInputs
    - Added a runtime error if an invalid value is manually entered or piped in
    - Would be nice if we could encode this "round down" behavior in the image_type
      expression of the output type, then we could get rid of the runtime error
    
    * Informative error message when failing to connect to SD
    
    * Remove "model checkpoint override" for now
    
    * Add "round down" behavior to the image_type of the outputs
    
    * lint

---
+++ backend/src/nodes/impl/external_stable_diffusion.py
@@ -0,0 +1,139 @@
+import base64
+import io
+import os
+from enum import Enum
+from typing import Dict, Union
+
+import cv2
+import numpy as np
+import requests
+from PIL import Image
+
+from .image_utils import normalize
+from ..utils.utils import get_h_w_c
+
+STABLE_DIFFUSION_HOST = os.environ.get("STABLE_DIFFUSION_HOST", "127.0.0.1")
+STABLE_DIFFUSION_PORT = os.environ.get("STABLE_DIFFUSION_PORT", "7860")
+
+STABLE_DIFFUSION_REQUEST_TIMEOUT = float(
+    os.environ.get("STABLE_DIFFUSION_REQUEST_TIMEOUT", "600")
+)  # 10 minutes
+
+STABLE_DIFFUSION_TEXT2IMG_URL = (
+    f"http://{STABLE_DIFFUSION_HOST}:{STABLE_DIFFUSION_PORT}/sdapi/v1/txt2img"
+)
+STABLE_DIFFUSION_IMG2IMG_URL = (
+    f"http://{STABLE_DIFFUSION_HOST}:{STABLE_DIFFUSION_PORT}/sdapi/v1/img2img"
+)
+STABLE_DIFFUSION_INTERROGATE_URL = (
+    f"http://{STABLE_DIFFUSION_HOST}:{STABLE_DIFFUSION_PORT}/sdapi/v1/interrogate"
+)
+STABLE_DIFFUSION_OPTIONS_URL = (
+    f"http://{STABLE_DIFFUSION_HOST}:{STABLE_DIFFUSION_PORT}/sdapi/v1/options"
+)
+
+ERROR_MSG = f"""
+If you want to use external stable diffusion nodes, run the Automatic1111 web ui with the --api flag, like so:
+
+./webui.sh --api
+
+ChaiNNer is currently configured to look for the API at http://{STABLE_DIFFUSION_HOST}:{STABLE_DIFFUSION_PORT}.  If you
+have it running somewhere else, you can change this using the STABLE_DIFFUSION_HOST and STABLE_DIFFUSION_PORT
+environment variables.
+"""
+
+TIMEOUT_MSG = f"""
+Stable diffusion request timeout reached.  Currently configured as {STABLE_DIFFUSION_REQUEST_TIMEOUT} seconds.  If you
+want to change this, set the STABLE_DIFFUSION_REQUEST_TIMEOUT environment variable.
+"""
+
+
+class ExternalServiceConnectionError(Exception):
+    pass
+
+
+class ExternalServiceTimeout(Exception):
+    pass
+
+
+def get(url) -> Dict:
+    try:
+        response = requests.get(url, timeout=STABLE_DIFFUSION_REQUEST_TIMEOUT)
+    except requests.ConnectionError as exc:
+        raise ExternalServiceConnectionError(ERROR_MSG) from exc
+    except requests.exceptions.ReadTimeout as exc:
+        raise ExternalServiceTimeout(TIMEOUT_MSG) from exc
+    return response.json()
+
+
+def post(url, json_data: Dict) -> Dict:
+    try:
+        response = requests.post(
+            url, json=json_data, timeout=STABLE_DIFFUSION_REQUEST_TIMEOUT
+        )
+    except requests.ConnectionError as exc:
+        raise ExternalServiceConnectionError(ERROR_MSG) from exc
+    except requests.exceptions.ReadTimeout as exc:
+        raise ExternalServiceTimeout(TIMEOUT_MSG) from exc
+    return response.json()
+
+
+def nearest_valid_size(width, height):
+    return (width // 8) * 8, (height // 8) * 8
+
+
+# Call the API at import time
+# If this fails (because the API isn't up) it will raise an exception and the
+# nodes importing this file won't be registered
+STABLE_DIFFUSION_OPTIONS = get(STABLE_DIFFUSION_OPTIONS_URL)
+
+
+def decode_base64_image(image_bytes: Union[bytes, str]) -> np.ndarray:
+    image = Image.open(io.BytesIO(base64.b64decode(image_bytes)))
+    image_nparray = np.array(image)
+    _, _, c = get_h_w_c(image_nparray)
+    if c == 3:
+        image_nparray = cv2.cvtColor(image_nparray, cv2.COLOR_RGB2BGR)
+    elif c == 4:
+        image_nparray = cv2.cvtColor(image_nparray, cv2.COLOR_RGBA2BGRA)
+    return normalize(image_nparray)
+
+
+def encode_base64_image(image_nparray: np.ndarray) -> str:
+    image_nparray = (np.clip(image_nparray, 0, 1) * 255).round().astype("uint8")
+    _, _, c = get_h_w_c(image_nparray)
+    if c == 1:
+        # PIL supports grayscale images just fine, so we don't need to do any conversion
+        pass
+    elif c == 3:
+        image_nparray = cv2.cvtColor(image_nparray, cv2.COLOR_BGR2RGB)
+    elif c == 4:
+        image_nparray = cv2.cvtColor(image_nparray, cv2.COLOR_BGRA2RGBA)
+    else:
+        raise RuntimeError
+    with io.BytesIO() as buffer:
+        with Image.fromarray(image_nparray) as image:
+            image.save(buffer, format="PNG")
+        return base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+
+class SamplerName(Enum):
+    EULER = "Euler"
+    EULER_A = "Euler a"
+    LMS = "LMS"
+    HEUN = "Heun"
+    DPM2 = "DPM2"
+    DPM2_A = "DPM2 a"
+    DPMpp_2S_A = "DPM++ 2S a"
+    DPMpp_2M = "DPM++ 2M"
+    DPMpp_SDE = "DPM++ SDE"
+    DPM_FAST = "DPM fast"
+    DPM_A = "DPM adaptive"
+    LMS_KARRAS = "LMS Karras"
+    DPM2_KARRAS = "DPM2 Karras"
+    DPM2_A_KARRAS = "DPM2 a Karras"
+    DPMpp_2S_A_KARRAS = "DPM++ 2S a Karras"
+    DPMpp_2M_KARRAS = "DPM++ 2M Karras"
+    DPMpp_SDE_KARRAS = "DPM++ SDE Karras"
+    DDIM = "DDIM"
+    PLMS = "PLMS"
