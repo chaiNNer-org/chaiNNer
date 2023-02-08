821b3c656f016523af111b261f89e8a433efd04c
Alex Dodge <alexdodge@gmail.com>
2023-02-07 16:53:21 -0800

    Dithering Node (#1502)
    * Dithering Node
    
    - Applies a Bayer ordered dithering filter
    
    * black
    
    * Move MAX_VALUES_BY_DTYPE to impl.image_utils
    
    * Use EnumInput for map size
    
    * Implement some more uniform dithering methods
    
    - Different threshold maps for ordered dithering
    - Error diffusion methods, including Floyd-Steinberg
    
    * Refactored, Riemersma dithering.
    
    * remove IM thresholds
    
    - they don't look great
    - I don't know about the licensing
    
    * non-uniform quantization with a palette
    
    - code still a bit messy and redundant
    - possibly some optimizations to be done
    
    * extract disinct colors from image
    
    - still a WIP
    
    * ...
    
    * Add type hints
    
    * black
    
    * k-means palette extraction
    
    * Remove color distance functions except for euclidean.
    
    - The others just don't seem to work as well
    - Less confusing UI
    
    * cleaning up the code
    
    * median cut
    
    * black
    
    * refactor and reformat
    
    * ...
    
    * ...
    
    * ...
    
    * ...
    
    * ...
    
    * ...
    
    * fixed a silly bug in the error diffusion.  (mixed up rows and columns)
    
    * ...
    
    * Use KD Tree to speed up color mapping
    
    - Also refactored things
    - Finally fixed the confusion around row/colum indexing in error diffusion
    
    * ...
    
    * Responding to feedback
    
    - Fixed formatting in constants.py
    - Renamed "LUT" to "Palette" in input label
    - Added fancy type hint to "Palette from Image"
    
    * typo
    
    * set openCV RNG seed, to make kmeans palettes deterministic
    
    * Lint
    
    * protecting against user asking for more colors than are in an image
    
    * formatting
    
    * Addressing new complaint in pyling 2.16
    
    * black
    
    * black upgrade
    
    * Fix type errors, kinda
    
    * change output name
    
    * ...
    
    ---------
    
    Co-authored-by: Joey Ballentine <joeyjballentine@gmail.com>

---
+++ backend/src/nodes/impl/dithering/common.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from ..image_utils import MAX_VALUES_BY_DTYPE
+
+
+def dtype_convert(image: np.ndarray, target_dtype: np.dtype):
+    if image.dtype == target_dtype:
+        return image
+
+    image = dtype_to_float(image)
+    return float_to_dtype(image, target_dtype)
+
+
+def dtype_to_float(image: np.ndarray) -> np.ndarray:
+    if image.dtype == np.dtype("float32"):
+        return image
+    max_value = MAX_VALUES_BY_DTYPE[image.dtype]
+    return image.astype(np.dtype("float32")) / max_value
+
+
+def dtype_to_uint8(image: np.ndarray) -> np.ndarray:
+    return dtype_convert(image, np.dtype("uint8"))
+
+
+def float_to_dtype(image: np.ndarray, dtype: np.dtype) -> np.ndarray:
+    if image.dtype == dtype:
+        return image
+    max_value = MAX_VALUES_BY_DTYPE[dtype]
+    return (image * max_value).astype(dtype)
+
+
+def apply_to_all_channels(
+    one_channel_filter, image: np.ndarray, *args, **kwargs
+) -> np.ndarray:
+    if image.ndim == 2:
+        return one_channel_filter(image, *args, **kwargs)
+    output_image = np.stack(
+        [
+            one_channel_filter(image[:, :, channel], *args, **kwargs)
+            for channel in range(image.shape[2])
+        ],
+        axis=2,
+    )
+    return output_image
---
+++ backend/src/nodes/nodes/image_filter/dither.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+from enum import Enum
+
+import numpy as np
+
+from . import category as ImageFilterCategory
+from ...impl.dithering.color_distance import batch_nearest_uniform_color
+from ...impl.dithering.constants import (
+    ErrorDiffusionMap,
+    ERROR_PROPAGATION_MAP_LABELS,
+    ThresholdMap,
+    THRESHOLD_MAP_LABELS,
+)
+from ...impl.dithering.diffusion import uniform_error_diffusion_dither
+from ...impl.dithering.ordered import ordered_dither
+from ...impl.dithering.riemersma import uniform_riemersma_dither
+from ...node_base import NodeBase, group
+from ...node_factory import NodeFactory
+from ...properties.inputs import ImageInput, NumberInput, EnumInput
+from ...properties.outputs import ImageOutput
+
+
+class UniformDitherAlgorithm(Enum):
+    NONE = "None"
+    ORDERED = "Ordered"
+    DIFFUSION = "Diffusion"
+    RIEMERSMA = "Riemersma"
+
+
+UNIFORM_DITHER_ALGORITHM_LABELS = {
+    UniformDitherAlgorithm.NONE: "No dithering",
+    UniformDitherAlgorithm.ORDERED: "Ordered Dithering",
+    UniformDitherAlgorithm.DIFFUSION: "Error Diffusion",
+    UniformDitherAlgorithm.RIEMERSMA: "Riemersma Dithering",
+}
+
+
+@NodeFactory.register("chainner:image:dither")
+class DitherNode(NodeBase):
+    def __init__(self):
+        super().__init__()
+        self.description = "Apply one of a variety of dithering algorithms with a uniform (evenly-spaced) palette."
+        self.inputs = [
+            ImageInput(),
+            NumberInput("Colors per channel", minimum=2, default=8),
+            EnumInput(
+                UniformDitherAlgorithm,
+                option_labels=UNIFORM_DITHER_ALGORITHM_LABELS,
+                default_value=UniformDitherAlgorithm.DIFFUSION,
+            ).with_id(2),
+            group(
+                "conditional-enum",
+                {
+                    "enum": 2,
+                    "conditions": [
+                        UniformDitherAlgorithm.ORDERED.value,
+                        UniformDitherAlgorithm.DIFFUSION.value,
+                        UniformDitherAlgorithm.RIEMERSMA.value,
+                    ],
+                },
+            )(
+                EnumInput(
+                    ThresholdMap,
+                    option_labels=THRESHOLD_MAP_LABELS,
+                    default_value=ThresholdMap.BAYER_16,
+                ).with_id(3),
+                EnumInput(
+                    ErrorDiffusionMap,
+                    option_labels=ERROR_PROPAGATION_MAP_LABELS,
+                    default_value=ErrorDiffusionMap.FLOYD_STEINBERG,
+                ).with_id(4),
+                NumberInput(
+                    "History Length",
+                    minimum=2,
+                    default=16,
+                ).with_id(5),
+            ),
+        ]
+        self.outputs = [ImageOutput(image_type="Input0")]
+        self.category = ImageFilterCategory
+        self.name = "Dither"
+        self.icon = "MdShowChart"
+        self.sub = "Quantize"
+
+    def run(
+        self,
+        img: np.ndarray,
+        num_colors: int,
+        dither_algorithm: UniformDitherAlgorithm,
+        threshold_map: ThresholdMap,
+        error_diffusion_map: ErrorDiffusionMap,
+        history_length: int,
+    ) -> np.ndarray:
+        if dither_algorithm == UniformDitherAlgorithm.NONE:
+            return batch_nearest_uniform_color(img, num_colors=num_colors)
+        elif dither_algorithm == UniformDitherAlgorithm.ORDERED:
+            return ordered_dither(
+                img, num_colors=num_colors, threshold_map=threshold_map
+            )
+        elif dither_algorithm == UniformDitherAlgorithm.DIFFUSION:
+            return uniform_error_diffusion_dither(
+                img, num_colors=num_colors, error_diffusion_map=error_diffusion_map
+            )
+        elif dither_algorithm == UniformDitherAlgorithm.RIEMERSMA:
+            return uniform_riemersma_dither(
+                img,
+                num_colors=num_colors,
+                history_length=history_length,
+                decay_ratio=1 / history_length,
+            )
