ea67dca71b379a51601e8922078e8b8fdc0ed303
Alex Dodge <alexdodge@gmail.com>
2023-02-09 13:36:26 -0800

    Change node to just output greyscale.
    - More modular
    - Can use a LUT to map colors if needed
    - Punts on the need for a color picker widget
    - NOTE: did this on my laptop without testing, so this might not actually run

--- backend/src/nodes/nodes/image_utility/create_gradient.py
+++ backend/src/nodes/nodes/image_utility/create_gradient.py
@@ -22,12 +22,6 @@ from ...properties.inputs import (
 from ...properties.outputs import ImageOutput
 
 
-class ColorMode(Enum):
-    RGB = 3
-    RGBA = 4
-    GRAYSCALE = 1
-
-
 class GradientStyle(Enum):
     HORIZONTAL = "Horizontal"
     VERTICAL = "Vertical"
@@ -78,157 +72,12 @@ class CreateGradientNode(NodeBase):
                     unit="deg",
                 ),
             ),
-            SliderInput(
-                "Middle Color Position",
-                minimum=0,
-                maximum=100,
-                default=50,
-                unit="%",
-            ),
-            EnumInput(ColorMode, default_value=ColorMode.GRAYSCALE).with_id(7),
-            group(
-                "conditional-enum",
-                {
-                    "enum": 7,
-                    "conditions": [
-                        [ColorMode.RGB.value, ColorMode.RGBA.value],
-                        [ColorMode.RGB.value, ColorMode.RGBA.value],
-                        [ColorMode.RGB.value, ColorMode.RGBA.value],
-                        [ColorMode.RGBA.value],
-                        [ColorMode.GRAYSCALE.value],
-                        [ColorMode.RGB.value, ColorMode.RGBA.value],
-                        [ColorMode.RGB.value, ColorMode.RGBA.value],
-                        [ColorMode.RGB.value, ColorMode.RGBA.value],
-                        [ColorMode.RGBA.value],
-                        [ColorMode.GRAYSCALE.value],
-                        [ColorMode.RGB.value, ColorMode.RGBA.value],
-                        [ColorMode.RGB.value, ColorMode.RGBA.value],
-                        [ColorMode.RGB.value, ColorMode.RGBA.value],
-                        [ColorMode.RGBA.value],
-                        [ColorMode.GRAYSCALE.value],
-                    ],
-                },
-            )(
-                SliderInput(
-                    "Red 1",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#ff0000"],
-                ),
-                SliderInput(
-                    "Green 1",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#00ff00"],
-                ),
-                SliderInput(
-                    "Blue 1",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#0000ff"],
-                ),
-                SliderInput(
-                    "Alpha 1",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#ffffff"],
-                ),
-                SliderInput(
-                    "Gray 1",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#ffffff"],
-                ),
-                SliderInput(
-                    "Red 2",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#ff0000"],
-                ),
-                SliderInput(
-                    "Green 2",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#00ff00"],
-                ),
-                SliderInput(
-                    "Blue 2",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#0000ff"],
-                ),
-                SliderInput(
-                    "Alpha 2",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#ffffff"],
-                ),
-                SliderInput(
-                    "Gray 2",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#ffffff"],
-                ),
-                SliderInput(
-                    "Red 3",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#ff0000"],
-                ),
-                SliderInput(
-                    "Green 3",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#00ff00"],
-                ),
-                SliderInput(
-                    "Blue 3",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#0000ff"],
-                ),
-                SliderInput(
-                    "Alpha 3",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#ffffff"],
-                ),
-                SliderInput(
-                    "Gray 3",
-                    minimum=0,
-                    maximum=255,
-                    default=0,
-                    gradient=["#000000", "#ffffff"],
-                ),
-            ),
         ]
         self.outputs = [
             ImageOutput(
-                image_type="""
-                Image {
-                    width: Input0,
-                    height: Input1,
-                    channels: match Input7 {
-                        ColorMode::Grayscale => 1,
-                        ColorMode::Rgb => 3,
-                        ColorMode::Rgba => 4,
-                    }
-                }
-                """
+                width="Input0",
+                height="Input1",
+                channels=1,
             )
         ]
         self.category = ImageUtilityCategory
@@ -244,71 +93,26 @@ class CreateGradientNode(NodeBase):
         inner_radius_percent: float,
         outer_radius_percent: float,
         conic_rotation: float,
-        middle_position: float,
-        color_mode: ColorMode,
-        red1: int,
-        green1: int,
-        blue1: int,
-        alpha1: int,
-        gray1: int,
-        red2: int,
-        green2: int,
-        blue2: int,
-        alpha2: int,
-        gray2: int,
-        red3: int,
-        green3: int,
-        blue3: int,
-        alpha3: int,
-        gray3: int,
     ) -> np.ndarray:
-        middle_position = middle_position / 100
-
-        img, color1, color2, color3 = None, None, None, None
-        if color_mode == ColorMode.RGB:
-            img = np.zeros((height, width, 3), dtype=np.float32)
-            color1 = np.array([blue1, green1, red1], dtype="float32") / 255
-            color2 = np.array([blue2, green2, red2], dtype="float32") / 255
-            color3 = np.array([blue3, green3, red3], dtype="float32") / 255
-        elif color_mode == ColorMode.RGBA:
-            img = np.zeros((height, width, 4), dtype=np.float32)
-            color1 = np.array([blue1, green1, red1, alpha1], dtype="float32") / 255
-            color2 = np.array([blue2, green2, red2, alpha2], dtype="float32") / 255
-            color3 = np.array([blue3, green3, red3, alpha3], dtype="float32") / 255
-        elif color_mode == ColorMode.GRAYSCALE:
-            img = np.zeros((height, width, 1), dtype=np.float32)
-            color1 = np.array([gray1], dtype="float32") / 255
-            color2 = np.array([gray2], dtype="float32") / 255
-            color3 = np.array([gray3], dtype="float32") / 255
+        img = np.zeros((height, width), dtype=np.float32)
 
         if gradient_style == GradientStyle.HORIZONTAL:
-            horizontal_gradient(img, color1, color2, color3, middle_position)
+            horizontal_gradient(img)
 
         elif gradient_style == GradientStyle.VERTICAL:
-            vertical_gradient(img, color1, color2, color3, middle_position)
+            vertical_gradient(img)
 
         elif gradient_style == GradientStyle.DIAGONAL:
-            diagonal_gradient(img, color1, color2, color3, middle_position)
+            diagonal_gradient(img)
 
         elif gradient_style == GradientStyle.RADIAL:
             radial_gradient(
                 img,
-                color1,
-                color2,
-                color3,
-                middle_position,
                 inner_radius_percent=inner_radius_percent / 100,
                 outer_radius_percent=outer_radius_percent / 100,
             )
 
         elif gradient_style == GradientStyle.CONIC:
-            conic_gradient(
-                img,
-                color1,
-                color2,
-                color3,
-                middle_position,
-                rotation=conic_rotation * np.pi / 180,
-            )
+            conic_gradient(img, rotation=conic_rotation * np.pi / 180)
 
         return img
