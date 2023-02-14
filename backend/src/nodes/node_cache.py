d3debce845e468ddd920cdff8e92925c92afe799
Alex Dodge <alexdodge@gmail.com>
2023-02-13 22:50:36 -0800

    ...

--- backend/src/nodes/node_cache.py
+++ backend/src/nodes/node_cache.py
@@ -1,5 +1,6 @@
 import functools
 import hashlib
+import time
 import os
 from enum import Enum
 from typing import List, Dict, Tuple, Optional
@@ -11,20 +12,11 @@ CACHE_MAX_BYTES = int(os.environ.get("CACHE_MAX_BYTES", 1024**3))  # default 1 G
 CACHE_REGISTRY: List["OutputCache"] = []
 
 
-class AccessCounter:
-    count = 0
-
-    @classmethod
-    def next(cls):
-        cls.count += 1
-        return cls.count
-
-
 class OutputCache:
     def __init__(self):
         self._data: Dict[Tuple, Tuple] = {}
-        self.bytes: Dict[Tuple, int] = {}
-        self.last_accessed: Dict[Tuple, int] = {}
+        self._bytes: Dict[Tuple, int] = {}
+        self._access_time: Dict[Tuple, float] = {}
 
         CACHE_REGISTRY.append(self)
 
@@ -53,27 +45,28 @@ class OutputCache:
                 size += out.nbytes
         return size
 
-    def oldest(self) -> Optional[Tuple[Tuple, int]]:
-        if not self.last_accessed:
+    def empty(self):
+        return len(self._data) == 0
+
+    def oldest(self) -> Optional[Tuple[Tuple, float]]:
+        if not self._access_time:
             return None
-        return min(self.last_accessed.items(), key=lambda x: x[1])
+        return min(self._access_time.items(), key=lambda x: x[1])
+
+    def size(self):
+        return sum(self._bytes.values())
 
     @staticmethod
     def _enforce_limits():
         while True:
-            total_bytes = sum(
-                [size for cache in CACHE_REGISTRY for size in cache.bytes.values()]
-            )
+            total_bytes = sum([cache.size() for cache in CACHE_REGISTRY])
             if total_bytes <= CACHE_MAX_BYTES:
                 return
             logger.info("Dropping oldest cache key")
 
-            oldest_keys = []
-            for cache in CACHE_REGISTRY:
-                x = cache.oldest()
-                if x is None:
-                    continue
-                oldest_keys.append((cache, x))
+            oldest_keys = [
+                (cache, cache.oldest()) for cache in CACHE_REGISTRY if not cache.empty()
+            ]
 
             cache, (key, _) = min(oldest_keys, key=lambda x: x[1][1])
             cache.drop(key)
@@ -82,7 +75,7 @@ class OutputCache:
         key = self._args_to_key(args)
         if key in self._data:
             logger.info("Cache hit")
-            self.last_accessed[key] = AccessCounter.next()
+            self._access_time[key] = time.time()
             return self._data[key]
         logger.info("Cache miss")
         return None
@@ -90,14 +83,14 @@ class OutputCache:
     def put(self, args, output):
         key = self._args_to_key(args)
         self._data[key] = output
-        self.bytes[key] = self._estimate_bytes(output)
-        self.last_accessed[key] = AccessCounter.next()
+        self._bytes[key] = self._estimate_bytes(output)
+        self._access_time[key] = time.time()
         self._enforce_limits()
 
     def drop(self, key):
         del self._data[key]
-        del self.bytes[key]
-        del self.last_accessed[key]
+        del self._bytes[key]
+        del self._access_time[key]
 
 
 def cached(run):
