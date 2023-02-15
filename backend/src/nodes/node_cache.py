dec46399b1b9d173a74a4eba73df9575ed895c1e
Alex Dodge <alexdodge@gmail.com>
2023-02-15 09:57:38 -0800

    use a temporary file to store cached numpy arrays

--- backend/src/nodes/node_cache.py
+++ backend/src/nodes/node_cache.py
@@ -1,7 +1,8 @@
 import functools
 import hashlib
-import time
 import os
+import tempfile
+import time
 from enum import Enum
 from typing import List, Dict, Tuple, Optional
 
@@ -9,12 +10,25 @@ import numpy as np
 from sanic.log import logger
 
 CACHE_MAX_BYTES = int(os.environ.get("CACHE_MAX_BYTES", 1024**3))  # default 1 GiB
-CACHE_REGISTRY: List["OutputCache"] = []
+CACHE_REGISTRY: List["NodeOutputCache"] = []
 
 
-class OutputCache:
+class CachedNumpyArray:
+    def __init__(self, arr: np.ndarray):
+        self.file = tempfile.TemporaryFile()
+        self.file.write(arr.tobytes())
+
+        self.shape = arr.shape
+        self.dtype = arr.dtype
+
+    def get(self) -> np.ndarray:
+        self.file.seek(0)
+        return np.frombuffer(self.file.read(), dtype=self.dtype).reshape(self.shape)
+
+
+class NodeOutputCache:
     def __init__(self):
-        self._data: Dict[Tuple, Tuple] = {}
+        self._data: Dict[Tuple, List] = {}
         self._bytes: Dict[Tuple, int] = {}
         self._access_time: Dict[Tuple, float] = {}
 
@@ -32,6 +46,7 @@ class OutputCache:
                 key.append(arg.value)
             elif isinstance(arg, np.ndarray):
                 key.append(tuple(arg.shape))
+                key.append(arg.dtype.str)
                 key.append(hashlib.sha256(arg.tobytes()).digest())
             else:
                 raise RuntimeError(f"Unexpected argument type {arg.__class__.__name__}")
@@ -43,6 +58,10 @@ class OutputCache:
         for out in output:
             if isinstance(out, np.ndarray):
                 size += out.nbytes
+            else:
+                # any other type but numpy arrays is probably negligible, but here's an overestimate to handle
+                # pathological cases where someone has a pipeline with a million math nodes
+                size += 1024  # 1 KiB
         return size
 
     def empty(self):
@@ -69,18 +88,44 @@ class OutputCache:
             cache, (key, _) = min(oldest_keys, key=lambda x: x[1][1])
             cache.drop(key)
 
-    def get(self, args) -> Optional[Tuple]:
+    @staticmethod
+    def _write_arrays_to_disk(output: List) -> List:
+        return [
+            CachedNumpyArray(item) if isinstance(item, np.ndarray) else item
+            for item in output
+        ]
+
+    @staticmethod
+    def _read_arrays_from_disk(output: List) -> List:
+        return [
+            item.get() if isinstance(item, CachedNumpyArray) else item
+            for item in output
+        ]
+
+    @staticmethod
+    def _output_to_list(output):
+        if not isinstance(output, (list, tuple)):
+            return [output]
+        return output
+
+    @staticmethod
+    def _list_to_output(output):
+        if len(output) == 1:
+            return output[0]
+        return output
+
+    def get(self, args) -> Optional[List]:
         key = self._args_to_key(args)
         if key in self._data:
             logger.info("Cache hit")
             self._access_time[key] = time.time()
-            return self._data[key]
+            return self._list_to_output(self._read_arrays_from_disk(self._data[key]))
         logger.info("Cache miss")
         return None
 
     def put(self, args, output):
         key = self._args_to_key(args)
-        self._data[key] = output
+        self._data[key] = self._write_arrays_to_disk(self._output_to_list(output))
         self._bytes[key] = self._estimate_bytes(output)
         self._access_time[key] = time.time()
         self._enforce_limits()
@@ -92,7 +137,7 @@ class OutputCache:
 
 
 def cached(run):
-    cache = OutputCache()
+    cache = NodeOutputCache()
 
     @functools.wraps(run)
     def _run(obj, *args):
