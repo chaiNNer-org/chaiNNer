651d85e099c4fc45e66362fad2f06e6fed8d762f
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2023-02-16 22:25:14 +0530

    separte dilate - erode node

--- backend/src/nodes/nodes/image_utility/erode.py
+++ backend/src/nodes/nodes/image_utility/erode.py
@@ -11,37 +11,31 @@ from ...properties.inputs import ImageInput, NumberInput, EnumInput
 from ...properties.outputs import ImageOutput
 
 
-class MorphOperation(Enum):
-    DILATE = "Dilate"
-    ERODE = "Erode"
-
-
 class MorphShape(Enum):
     RECTANGLE = cv2.MORPH_RECT
     ELLIPSE = cv2.MORPH_ELLIPSE
     CROSS = cv2.MORPH_CROSS
 
 
-@NodeFactory.register("chainner:image:dilate_erode")
-class DilateErodeNode(NodeBase):
+@NodeFactory.register("chainner:image:erode")
+class ErodeNode(NodeBase):
     def __init__(self):
         super().__init__()
-        self.description = "Dilate or Erode an image"
+        self.description = "Erode an image"
         self.inputs = [
             ImageInput(),
-            EnumInput(MorphOperation),
             EnumInput(
                 MorphShape,
                 option_labels={
-                    MorphShape.RECTANGLE: "Rectangle",
-                    MorphShape.ELLIPSE: "Ellipse",
+                    MorphShape.RECTANGLE: "Square",
+                    MorphShape.ELLIPSE: "Circle",
                     MorphShape.CROSS: "Cross",
                 },
             ),
             NumberInput(
-                "Size",
+                "Radius",
                 minimum=0,
-                default=5,
+                default=1,
                 controls_step=1,
             ),
             NumberInput(
@@ -53,26 +47,23 @@ class DilateErodeNode(NodeBase):
         ]
         self.outputs = [ImageOutput(image_type="Input0")]
         self.category = ImageUtilityCategory
-        self.name = "Dilate/Erode"
+        self.name = "Erode"
         self.icon = "MdOutlineAutoFixHigh"
         self.sub = "Miscellaneous"
 
     def run(
         self,
         img: np.ndarray,
-        morph_operation: MorphOperation,
         morph_shape: MorphShape,
-        size: int,
+        radius: int,
         iterations: int,
     ) -> np.ndarray:
-        """Dilate or Erode an image"""
+        """Erode an image"""
 
-        if size == 0 or iterations == 0:
+        if radius == 0 or iterations == 0:
             return img
 
+        size = 2 * radius + 1
         element = cv2.getStructuringElement(morph_shape.value, (size, size))
-        operation = (
-            cv2.dilate if morph_operation == MorphOperation.DILATE else cv2.erode
-        )
 
-        return operation(img, element, iterations=iterations)
+        return cv2.erode(img, element, iterations=iterations)
