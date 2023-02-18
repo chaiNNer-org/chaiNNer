fa0b075195135502dc79967b0a48976251b8d8a7
theflyingzamboni <55669985+theflyingzamboni@users.noreply.github.com>
2023-02-18 13:34:44 -0500

    PyTorch Inpaint Node (#1569)
    * Initial
    
    * Removed torchscript lama code
    
    * LaMa working as PyTorch node
    
    * Fixed channel restriction on image
    
    * Removed unused code from lama model
    
    * Added pylint skip to arch code
    
    * Fixed black and formatting
    
    * Update backend/src/nodes/nodes/pytorch/inpaint.py
    
    Hardcode 3-channel output
    
    Co-authored-by: Michael Schmidt <msrd0000@gmail.com>
    
    * Changed mask binarize threshold to 0.5
    
    * Fixed PR suggestion
    
    ---------
    
    Co-authored-by: Michael Schmidt <msrd0000@gmail.com>

--- backend/src/nodes/impl/pytorch/model_loading.py
+++ backend/src/nodes/impl/pytorch/model_loading.py
@@ -1,15 +1,16 @@
 from sanic.log import logger
 
+from .architecture.face.codeformer import CodeFormer
+from .architecture.face.gfpganv1_clean_arch import GFPGANv1Clean
+from .architecture.face.restoreformer_arch import RestoreFormer
+from .architecture.HAT import HAT
+from .architecture.LaMa import LaMa
 from .architecture.RRDB import RRDBNet as ESRGAN
 from .architecture.SPSR import SPSRNet as SPSR
 from .architecture.SRVGG import SRVGGNetCompact as RealESRGANv2
 from .architecture.SwiftSRGAN import Generator as SwiftSRGAN
-from .architecture.SwinIR import SwinIR
 from .architecture.Swin2SR import Swin2SR
-from .architecture.HAT import HAT
-from .architecture.face.gfpganv1_clean_arch import GFPGANv1Clean
-from .architecture.face.restoreformer_arch import RestoreFormer
-from .architecture.face.codeformer import CodeFormer
+from .architecture.SwinIR import SwinIR
 from .types import PyTorchModel
 
 
@@ -68,6 +69,12 @@ def load_state_dict(state_dict) -> PyTorchModel:
         and "quantize.embedding.weight" in state_dict_keys
     ):
         model = CodeFormer(state_dict)
+    # LaMa
+    elif (
+        "model.model.1.bn_l.running_mean" in state_dict_keys
+        or "generator.model.1.bn_l.running_mean" in state_dict_keys
+    ):
+        model = LaMa(state_dict)
     # Regular ESRGAN, "new-arch" ESRGAN, Real-ESRGAN v1
     else:
         try:
