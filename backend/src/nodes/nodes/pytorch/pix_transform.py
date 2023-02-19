12d15b11fa6ba28570756163cc63cfcf3eb452e6
Michael Schmidt <mitchi5000.ms@googlemail.com>
2023-02-19 12:01:55 +0100

    Added tile-based auto split

--- backend/src/nodes/nodes/pytorch/pix_transform.py
+++ backend/src/nodes/nodes/pytorch/pix_transform.py
@@ -3,16 +3,14 @@ from __future__ import annotations
 import numpy as np
 import torch
 
-from ...impl.image_op import to_op
-from ...impl.pytorch.pix_transform.pix_transform import DEFAULT_PARAMS, PixTransform
+from ...impl.pytorch.pix_transform.auto_split import pix_transform_auto_split
+from ...impl.pytorch.pix_transform.pix_transform import DEFAULT_PARAMS
 from ...impl.pytorch.utils import to_pytorch_execution_options
-from ...impl.upscale.grayscale import grayscale_split
 from ...node_base import NodeBase
 from ...node_factory import NodeFactory
 from ...properties.inputs import ImageInput, SliderInput
 from ...properties.outputs import ImageOutput
 from ...utils.exec_options import get_execution_options
-from ...utils.utils import get_h_w_c
 from . import category as PyTorchCategory
 
 
@@ -69,22 +67,11 @@ class PixTransformNode(NodeBase):
         self.sub = "Processing"
 
     def run(self, source: np.ndarray, guide: np.ndarray, iterations: int) -> np.ndarray:
-        s_h, s_w, _ = get_h_w_c(source)
-        g_h, g_w, _ = get_h_w_c(guide)
-
-        assert (
-            g_h > s_h and g_w > s_w
-        ), f"The guide image mus be larger than the source image."
-        assert (
-            g_w / s_w == g_w // s_w and g_w / s_w == g_h / s_h
-        ), "The size of the guide image must be an integer multiple of the size of the source image (e.g. 2x, 3x, 4x, ...)."
-
         exec_options = to_pytorch_execution_options(get_execution_options())
 
-        pix = to_op(PixTransform)(
-            np.transpose(guide, (2, 0, 1)),
+        return pix_transform_auto_split(
+            source=source,
+            guide=guide,
             device=torch.device(exec_options.full_device),
             params={**DEFAULT_PARAMS, "iteration": iterations * 1000},
         )
-
-        return grayscale_split(source, pix)
