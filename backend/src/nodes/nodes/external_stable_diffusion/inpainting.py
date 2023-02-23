88fd3e6be16e2993af29594b8bf0cf1cfc890911
Alex Dodge <alexdodge@gmail.com>
2023-02-22 21:19:47 -0800

    ...

--- backend/src/nodes/nodes/external_stable_diffusion/inpainting.py
+++ backend/src/nodes/nodes/external_stable_diffusion/inpainting.py
@@ -9,7 +9,7 @@ from ...groups import conditional_group
 from ...impl.external_stable_diffusion import (
     RESIZE_MODE_LABELS,
     SAMPLER_NAME_LABELS,
-    STABLE_DIFFUSION_IMG2IMG_URL,
+    STABLE_DIFFUSION_IMG2IMG_PATH,
     InpaintingFill,
     ResizeMode,
     SamplerName,
@@ -183,7 +183,7 @@ class Img2Img(NodeBase):
             "resize_mode": resize_mode.value,
             "tiling": tiling,
         }
-        response = post(url=STABLE_DIFFUSION_IMG2IMG_URL, json_data=request_data)
+        response = post(path=STABLE_DIFFUSION_IMG2IMG_PATH, json_data=request_data)
         result = decode_base64_image(response["images"][0])
         h, w, _ = get_h_w_c(result)
         if inpaint_area == InpaintArea.ONLY_MASKED:
