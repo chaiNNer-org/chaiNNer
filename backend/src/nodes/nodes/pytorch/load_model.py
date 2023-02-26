a8ef567614d6388df0946f7ff283756f74adbef8
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-02-26 11:50:40 -0500

    Default all models to have supports_fp16 attribute (set to false) (#1600)

--- backend/src/nodes/nodes/pytorch/load_model.py
+++ backend/src/nodes/nodes/pytorch/load_model.py
@@ -70,6 +70,8 @@ class LoadModelNode(NodeBase):
                 v.requires_grad = False
             model.eval()
             model = model.to(torch.device(exec_options.full_device))
+            if not hasattr(model, "supports_fp16"):
+                model.supports_fp16 = False  # type: ignore
             should_use_fp16 = exec_options.fp16 and model.supports_fp16
             if should_use_fp16:
                 model = model.half()
