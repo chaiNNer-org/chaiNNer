3f5f95c23d65fea7f40c5b9219b805e2b92c0dd5
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-02-28 13:27:55 +0100

    Improved exact auto split

--- backend/src/nodes/impl/upscale/auto_split.py
+++ backend/src/nodes/impl/upscale/auto_split.py
@@ -62,9 +62,6 @@ def _exact_split(
     overlap: int,
 ) -> np.ndarray:
     h, w, c = get_h_w_c(img)
-    logger.info(
-        f"Exact size split image ({w}x{h}px @ {c}) with exact tile size {starting_tile_size[0]}x{starting_tile_size[1]}px."
-    )
 
     def no_split_upscale(i: np.ndarray, r: Region) -> np.ndarray:
         result = upscale(i, r)
@@ -75,11 +72,15 @@ def _exact_split(
     MAX_ITER = 20
     for _ in range(MAX_ITER):
         try:
+            logger.info(
+                f"Exact size split image ({w}x{h}px @ {c}) with exact tile size {starting_tile_size[0]}x{starting_tile_size[1]}px."
+            )
+            max_overlap = min(*starting_tile_size) // 4
             return exact_split(
                 img=img,
                 exact_size=starting_tile_size,
                 upscale=no_split_upscale,
-                overlap=overlap,
+                overlap=min(max_overlap, overlap),
             )
         except _SplitEx:
             starting_tile_size = split_tile_size(starting_tile_size)
