891f051930a28060b5934d643251876dd16bac74
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2023-03-06 20:23:52 +0530

    Create high_pass.py

---
+++ backend/src/nodes/nodes/image_filter/high_pass.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import cv2
+import numpy as np
+
+from ...node_base import NodeBase
+from ...node_factory import NodeFactory
+from ...properties.inputs import ImageInput, NumberInput
+from ...properties.outputs import ImageOutput
+from . import category as ImageFilterCategory
+
+
+@NodeFactory.register("chainner:image:high_pass")
+class HighPassFilterNode(NodeBase):
+    def __init__(self):
+        super().__init__()
+        self.description = "Apply High Pass filter on image"
+        self.inputs = [
+            ImageInput(),
+            NumberInput("Radius", minimum=0, default=1, precision=2, controls_step=1),
+        ]
+        self.outputs = [ImageOutput(image_type="Input0")]
+        self.category = ImageFilterCategory
+        self.name = "High Pass"
+        self.icon = "MdOutlineAutoFixHigh"
+        self.sub = "Miscellaneous"
+
+    def run(
+        self,
+        img: np.ndarray,
+        radius: float,
+    ) -> np.ndarray:
+        """High Pass filter"""
+
+        if radius == 0:
+            return img * 0 + 0.5
+        else:
+            return np.clip(
+                img - cv2.GaussianBlur(img, (0, 0), sigmaX=radius, sigmaY=radius) + 0.5,
+                0,
+                1,
+            )
