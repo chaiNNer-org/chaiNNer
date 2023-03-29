dfdb7ffe65785a2804a413668e0483a7063a6778
Joey Ballentine <joeyjballentine@gmail.com>
2023-03-29 00:58:03 -0400

    remove transferred over nodes

--- backend/src/nodes/nodes/image_channel/__init__.py
+++
@@ -1,8 +0,0 @@
-from ...category import Category
-
-category = Category(
-    name="Image (Channels)",
-    description="Nodes that deal with manipulating channels of images.",
-    icon="MdAllOut",
-    color="#D69E2E",
-)
