a008908acc7d1147a0c7d83638256648f842c0cd
theflyingzamboni <theaerialzamboni@gmail.com>
2023-04-05 18:49:19 -0400

    Add ISNet support and fix normalization bug

--- backend/src/nodes/impl/rembg/session_base.py
+++ backend/src/nodes/impl/rembg/session_base.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Tuple
 
+import cv2
 import numpy as np
 import onnxruntime as ort
 from PIL import Image
@@ -7,25 +8,27 @@ from PIL.Image import Image as PILImage
 
 
 class BaseSession:
-    def __init__(self, inner_session: ort.InferenceSession):
-        self.inner_session = inner_session
-
-    def normalize(
+    def __init__(
         self,
-        img: PILImage,
+        inner_session: ort.InferenceSession,
         mean: Tuple[float, float, float],
         std: Tuple[float, float, float],
         size: Tuple[int, int],
-    ) -> Dict[str, np.ndarray]:
-        im = img.convert("RGB").resize(size, Image.LANCZOS)
+    ):
+        self.inner_session = inner_session
+        self.mean = mean
+        self.std = std
+        self.size = size
 
+    def normalize(self, img: PILImage) -> Dict[str, np.ndarray]:
+        im = img.convert("RGB").resize(self.size, Image.LANCZOS)
         im_ary = np.array(im)
         im_ary = im_ary / np.max(im_ary)
 
         tmpImg = np.zeros((im_ary.shape[0], im_ary.shape[1], 3))
-        tmpImg[:, :, 0] = (im_ary[:, :, 0] - mean[0]) / std[0]
-        tmpImg[:, :, 1] = (im_ary[:, :, 1] - mean[1]) / std[1]
-        tmpImg[:, :, 2] = (im_ary[:, :, 2] - mean[2]) / std[2]
+        tmpImg[:, :, 0] = (im_ary[:, :, 0] - self.mean[0]) / self.std[0]
+        tmpImg[:, :, 1] = (im_ary[:, :, 1] - self.mean[1]) / self.std[1]
+        tmpImg[:, :, 2] = (im_ary[:, :, 2] - self.mean[2]) / self.std[2]
 
         tmpImg = tmpImg.transpose((2, 0, 1))
 
--- backend/src/nodes/impl/rembg/session_factory.py
+++ backend/src/nodes/impl/rembg/session_factory.py
@@ -11,9 +11,27 @@ from .session_simple import SimpleSession
 def new_session(session: ort.InferenceSession) -> BaseSession:
     session_class: Type[BaseSession]
 
-    if get_input_shape(session)[2] == 768:
+    input_width = get_input_shape(session)[2]
+
+    # Using size to determine session type and norm parameters is fragile,
+    # but at the moment I don't know a better way to detect architecture due
+    # to the lack of consistency in naming and outputs across arches and repos.
+    # It works right now because of the limited number of models supported,
+    # but if that expands, it may become necessary to find an alternative.
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.224, 0.225)
+    size = (input_width, input_width) if input_width is not None else (320, 320)
+    if input_width == 768:  # U2NET cloth model
         session_class = ClothSession
+        mean = (0.5, 0.5, 0.5)
+        std = (0.5, 0.5, 0.5)
     else:
         session_class = SimpleSession
+        if input_width == 1024:  # ISNET
+            mean = (0.5, 0.5, 0.5)
+            std = (1, 1, 1)
+        elif input_width == 512:  # Models trained using anime-segmentation repo
+            mean = (0, 0, 0)
+            std = (1, 1, 1)
 
-    return session_class(session)
+    return session_class(session, mean, std, size)
