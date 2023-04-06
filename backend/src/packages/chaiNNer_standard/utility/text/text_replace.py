c9f37675421e377b7d70f7d37b43a3367f11b563
Michael Schmidt <mitchi5000.ms@googlemail.com>
2023-04-06 03:57:58 +0200

    Added Text Replace node (#1704)
    * Added Text Replace node
    
    * Formatting

---
+++ backend/src/packages/chaiNNer_standard/utility/text/text_replace.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from enum import Enum
+
+from nodes.properties.inputs import EnumInput, TextInput
+from nodes.properties.outputs import TextOutput
+
+from .. import text_group
+
+
+class ReplacementMode(Enum):
+    REPLACE_ALL = 0
+    REPLACE_FIRST = 1
+
+
+@text_group.register(
+    schema_id="chainner:utility:text_replace",
+    name="Text Replace",
+    description="Replace occurrences of some text with a replacement text. Either or all occurrences or the first occurrence will be replaced",
+    icon="MdTextFields",
+    inputs=[
+        TextInput("Text", min_length=0),
+        TextInput("Old Text", min_length=0),
+        TextInput("Replacement", min_length=0),
+        EnumInput(
+            ReplacementMode,
+            label="Replace mode",
+            default_value=ReplacementMode.REPLACE_ALL,
+        ),
+    ],
+    outputs=[
+        TextOutput(
+            "Text",
+            output_type="""
+                let count = match Input3 {
+                    ReplacementMode::ReplaceAll => inf,
+                    ReplacementMode::ReplaceFirst => 1,
+                };
+                string::replace(Input0, Input1, Input2, count)
+            """,
+        ),
+    ],
+)
+def text_replace(text: str, old: str, new: str, mode: ReplacementMode) -> str:
+    if mode == ReplacementMode.REPLACE_ALL:
+        return text.replace(old, new)
+    else:
+        return text.replace(old, new, 1)
