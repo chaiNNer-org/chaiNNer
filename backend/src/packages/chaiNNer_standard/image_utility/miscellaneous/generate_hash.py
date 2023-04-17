28c03d572374a1da736357e27988e2ac637be559
Zarxrax <zarxrax@yahoo.com>
2023-04-17 11:01:59 -0400

    add generate_hash node (#1738)
    * Add files via upload
    
    * Update generate_hash.py
    
    add conversion to uint8
    
    * Update backend/src/packages/chaiNNer_standard/image_utility/miscellaneous/generate_hash.py
    
    Co-authored-by: Michael Schmidt <msrd0000@gmail.com>
    
    ---------
    
    Co-authored-by: Michael Schmidt <msrd0000@gmail.com>

---
+++ backend/src/packages/chaiNNer_standard/image_utility/miscellaneous/generate_hash.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import base64
+import hashlib
+from typing import Tuple
+
+import numpy as np
+
+from nodes.impl.image_utils import to_uint8
+from nodes.properties.inputs import ImageInput, SliderInput
+from nodes.properties.outputs import TextOutput
+
+from .. import miscellaneous_group
+
+
+@miscellaneous_group.register(
+    schema_id="chainner:image:generate_hash",
+    name="Generate Hash",
+    description="Generate a hash from an image using the BLAKE2b hashing algorithm.",
+    icon="MdCalculate",
+    inputs=[
+        ImageInput(),
+        SliderInput(
+            "Digest Size (in bytes)",
+            minimum=1,
+            maximum=64,
+            default=8,
+            precision=1,
+            controls_step=1,
+        ),
+    ],
+    outputs=[
+        TextOutput("Hex"),
+        TextOutput("Base64"),
+    ],
+)
+def run(img: np.ndarray, size: int) -> Tuple[str, str]:
+    """Generate a hash from the input image. The digest size determines the length of the hash that is output."""
+    img = to_uint8(img)
+    h = hashlib.blake2b(img, digest_size=size)  # type: ignore
+    return h.hexdigest(), base64.urlsafe_b64encode(h.digest()).decode("utf-8")
