4f29c9b2456f0b29d9b579faafc3026a6e41d26e
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2023-05-05 11:31:37 +0530

    Update pixel_blur.py
    added padding

--- backend/src/packages/chaiNNer_standard/image_filter/blur/pixel_blur.py
+++ backend/src/packages/chaiNNer_standard/image_filter/blur/pixel_blur.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import cv2
 import numpy as np
+
 from nodes.properties.inputs import ImageInput, NumberInput
 from nodes.properties.outputs import ImageOutput
 
@@ -27,12 +29,14 @@ def pixel_blur_node(
     block_sizes = (size_x, size_y)
     height, width = img.shape[:2]
 
+    pad_x = (block_sizes[0] - width % block_sizes[0]) % block_sizes[0]
+    pad_y = (block_sizes[1] - height % block_sizes[1]) % block_sizes[1]
+    img = cv2.copyMakeBorder(img, 0, pad_y, 0, pad_x, cv2.BORDER_REFLECT_101)
+
     num_blocks_x = width // block_sizes[0]
     num_blocks_y = height // block_sizes[1]
 
-    blocks = img[
-        : num_blocks_y * block_sizes[1], : num_blocks_x * block_sizes[0]
-    ].reshape((num_blocks_y, block_sizes[1], num_blocks_x, block_sizes[0], 3))
+    blocks = img[:num_blocks_y * block_sizes[1], :num_blocks_x * block_sizes[0]].reshape((num_blocks_y, block_sizes[1], num_blocks_x, block_sizes[0], img.shape[-1]))
 
     average_colors = np.mean(np.mean(blocks, axis=1), axis=2)
 
@@ -40,4 +44,4 @@ def pixel_blur_node(
         np.repeat(average_colors, block_sizes[0], axis=1), block_sizes[1], axis=0
     )
 
-    return repeated_colors
+    return repeated_colors[:height, :width]
