a535784fa8a91eb927f245897c6ff4488b18eb0d
Joey Ballentine <joeyjballentine@gmail.com>
2023-05-25 23:41:24 -0400

    lint

--- src/common/dependencies.ts
+++ src/common/dependencies.ts
@@ -1,11 +1,5 @@
 import { Version } from './common-types';
 
-const KB = 1024 ** 1;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const MB = 1024 ** 2;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const GB = 1024 ** 3;
-
 export interface PyPiPackage {
     displayName?: string;
     packageName: string;
