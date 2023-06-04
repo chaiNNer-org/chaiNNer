7c85cdfae60fe711223877db0a1e228b5b455952
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-06-04 20:23:45 +0200

    Added bias option

--- backend/src/nodes/impl/cas.py
+++ backend/src/nodes/impl/cas.py
@@ -15,7 +15,7 @@ def _luminance(img: np.ndarray) -> np.ndarray:
     return np.dot(img[..., :3], [0.2126, 0.7152, 0.0722])
 
 
-def create_cas_mask(img: np.ndarray, kernel) -> np.ndarray:
+def create_cas_mask(img: np.ndarray, kernel, bias: float = 1) -> np.ndarray:
     """
     Uses contrast adaptive sharpening's method to create a mask to interpolate between the original
     and sharpened image.
@@ -23,10 +23,19 @@ def create_cas_mask(img: np.ndarray, kernel) -> np.ndarray:
     `kernel` is an element create by `cv2.getStructuringElement`. It determines the shape and size
     of each pixel's neighborhood.
 
+    `bias` is used to bias the mask towards the more sharpening. A value of 1 means no bias, a
+    value greater than 1 biases the mask towards the sharpened image, a value less than 1 biases
+    the mask towards the original image.
+
+    Note: The original work uses a bias of 2.0. We use a bias default of 1 to make the sharpening
+    less aggressive. This is more useful for chaiNNer, because users can blend with a
+    non-CAS-sharpened image to get a similar effect.
+
     Reference:
     Lou Kramer, FidelityFX CAS, AMD Developer Day 2019, https://gpuopen.com/wp-content/uploads/2019/07/FidelityFX-CAS.pptx
     https://www.shadertoy.com/view/wtlSWB#
     """
+    assert bias > 0, "Bias must be greater than or equal to 0."
 
     l = _luminance(img)
     min_l = cv2.erode(l, kernel)
@@ -35,12 +44,17 @@ def create_cas_mask(img: np.ndarray, kernel) -> np.ndarray:
     max_l += 1e-8
     min_d /= max_l
     mask = min_d
-    # no sqrt, because we only need that in linear color space
-    # mask = np.sqrt(mask, out=mask)
+    if bias != 1:
+        mask = np.power(mask, 1 / bias, out=mask)
     return mask
 
 
-def cas_mix(img: np.ndarray, sharpened: np.ndarray, kernel) -> np.ndarray:
-    mask = create_cas_mask(img, kernel)
+def cas_mix(
+    img: np.ndarray,
+    sharpened: np.ndarray,
+    kernel,
+    bias: float = 1,
+) -> np.ndarray:
+    mask = create_cas_mask(img, kernel, bias)
     mask = np.dstack((mask,) * get_h_w_c(sharpened)[2])
     return img * (1 - mask) + sharpened * mask
