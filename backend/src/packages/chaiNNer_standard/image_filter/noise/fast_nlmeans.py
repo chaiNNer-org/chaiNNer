a2f09eb47cd97de8f329827bc9684409420ef2fe
Arnaud de Gérard <arnaud.degerard@gmail.com>
2023-06-23 19:38:54 +0200

    - correction: alpha channel - added test file

--- backend/src/packages/chaiNNer_standard/image_filter/noise/fast_nlmeans.py
+++ backend/src/packages/chaiNNer_standard/image_filter/noise/fast_nlmeans.py
@@ -6,6 +6,7 @@ import cv2
 from nodes.properties.inputs import ImageInput, SliderInput, NumberInput
 from nodes.properties.outputs import ImageOutput
 from nodes.impl.image_utils import to_uint8
+from nodes.utils.utils import get_h_w_c
 
 from .. import noise_group
 
@@ -63,15 +64,23 @@ def fast_nlmeans_node(
     if not (search_window_size % 2):
         raise ValueError("\'Search window size\' must be an odd value.")
 
+    _, _, c = get_h_w_c(img)
     image_array = to_uint8(img)
 
-    if img.shape[-1] == 1:
+    if c == 1:
         denoised = cv2.fastNlMeansDenoising(src=image_array, h=h,
             templateWindowSize=template_window_size, searchWindowSize=search_window_size)
 
     else:
-        denoised = cv2.fastNlMeansDenoisingColored(src=image_array, h=h, hColor=h_color,
+        rgb = image_array[:, :, :3]
+        if c == 4:
+            alpha = image_array[:, :, 3]
+
+        denoised = cv2.fastNlMeansDenoisingColored(src=rgb, h=h, hColor=h_color,
             templateWindowSize=template_window_size, searchWindowSize=search_window_size)
+
+        if c == 4:
+            denoised = np.dstack((denoised, alpha))
 
     return denoised
 
