bbeee1b16b9dc56ab4fa748d1ca8ab728b8e507a
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-06-24 14:50:32 +0000

    lint

--- backend/src/dependencies/store.py
+++ backend/src/dependencies/store.py
@@ -197,6 +197,7 @@ async def install_dependencies(
         version = dep_info["version"]
         installed_packages[installing_name] = version
 
+
 __all__ = [
     "DependencyInfo",
     "python_path",
