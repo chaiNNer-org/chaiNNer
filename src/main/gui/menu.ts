5603c30e9c7d0fbea6fb91224c83512260422328
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-07-16 13:21:00 -0400

    Remove upscale wiki menu link (#1941)

--- src/main/gui/menu.ts
+++ src/main/gui/menu.ts
@@ -236,12 +236,6 @@ export const setMainMenu = ({ mainWindow, menuData, enabled = false }: MainMenuA
                         await shell.openPath(getLogsFolder());
                     },
                 },
-                {
-                    label: 'Get Models (Upscale Wiki)',
-                    click: async () => {
-                        await shell.openExternal('https://upscale.wiki/wiki/Model_Database');
-                    },
-                },
                 { type: 'separator' },
                 {
                     label: 'About chaiNNer',
