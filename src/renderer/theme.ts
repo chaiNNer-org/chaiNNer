17c5de5225a7c640e84617fad8505d8c93624e92
Toni Förster <toni.foerster@gmail.com>
2023-08-09 00:08:09 +0200

    changes to theme selection (#2060)
    instead of a toggle for always dark the user can now choose between:
    
    Dark Mode / Light Mode / System

--- src/renderer/theme.ts
+++ src/renderer/theme.ts
@@ -1,8 +1,28 @@
 import { Theme, extendTheme } from '@chakra-ui/react';
 
-const config = {
+// This is the initial theme config on startup
+const dark = {
     initialColorMode: 'dark',
     useSystemColorMode: false,
+} as const;
+
+// TODO: This should be used later after reading the theme settings.
+// When light, change the theme to this values before displaying the
+// window. Need to figure out where and when to load the new theme.
+// Currently this does nothing.
+const light = {
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+} as const;
+
+// TODO: This should be used later to dynamically change the theme,
+// when the OS changes from dark to light or vice versa. Need to
+// figure out where and when to load the new theme. Currently this
+// does nothing.
+
+const system = {
+    initialColorMode: 'system',
+    useSystemColorMode: true,
 } as const;
 
 const grays = [50, 100, 200, 300, 400, 500, 600, 650, 700, 750, 800, 850, 900];
@@ -16,4 +36,8 @@ const fonts = {
     monospace: `Roboto-Mono, monospace`,
 };
 
-export const theme = extendTheme({ config, colors, fonts } as const) as Theme;
+export const darktheme = extendTheme({ config: dark, colors, fonts } as const) as Theme;
+
+export const lighttheme = extendTheme({ config: light, colors, fonts } as const) as Theme;
+
+export const systemtheme = extendTheme({ config: system, colors, fonts } as const) as Theme;
