673a4a4ce23199db26a60333819c2839e631a4cb
Michael Schmidt <mitchi5000.ms@googlemail.com>
2023-08-12 20:11:38 +0200

    Use Rust implementation for clipboard (#2079)
    * Use Rust implementation for clipboard
    
    * chainner_ext 0.3.0

--- backend/src/packages/chaiNNer_standard/utility/clipboard/copy_to_clipboard.py
+++ backend/src/packages/chaiNNer_standard/utility/clipboard/copy_to_clipboard.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
-from typing import Union
-
 import numpy as np
+from chainner_ext import Clipboard
 
-from nodes.impl import clipboard
 from nodes.properties.inputs import ClipboardInput
 
 from .. import clipboard_group
@@ -13,10 +11,7 @@ from .. import clipboard_group
 @clipboard_group.register(
     schema_id="chainner:utility:copy_to_clipboard",
     name="Copy To Clipboard",
-    description=[
-        "Copies the input to the clipboard.",
-        "Currently does not work on Arm MacOS.",
-    ],
+    description="Copies the input to the clipboard.",
     icon="BsClipboard",
     inputs=[
         ClipboardInput(),
@@ -24,10 +19,8 @@ from .. import clipboard_group
     outputs=[],
     side_effects=True,
 )
-def copy_to_clipboard_node(value: Union[str, np.ndarray]) -> None:
+def copy_to_clipboard_node(value: str | np.ndarray) -> None:
     if isinstance(value, np.ndarray):
-        clipboard.copy_image(value)
-    elif isinstance(value, str):  # type: ignore
-        clipboard.copy_text(value)
+        Clipboard.create_instance().write_image(value, "BGR")
     else:
-        raise RuntimeError(f"Unsupported type {type(value)}")
+        Clipboard.create_instance().write_text(value)
