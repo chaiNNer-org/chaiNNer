158a78f565f5793a894059061f5641acb51acb14
Arnaud de Gérard <arnaud.degerard@gmail.com>
2023-08-24 20:20:10 +0200

    Simplify node: - removed font selection - added bold/italic checkbox - improved test project

--- backend/src/packages/chaiNNer_standard/image/create_images/text_as_image.py
+++ backend/src/packages/chaiNNer_standard/image/create_images/text_as_image.py
@@ -10,81 +10,24 @@ from PIL import Image, ImageDraw, ImageFont
 from nodes.impl.color.color import Color
 from nodes.impl.image_utils import normalize
 from nodes.impl.pil_utils import convert_to_BGRA
-from nodes.properties.inputs import ColorInput, EnumInput, NumberInput, TextInput
+from nodes.properties.inputs import (
+    BoolInput,
+    ColorInput,
+    EnumInput,
+    NumberInput,
+    TextInput,
+)
 from nodes.properties.outputs import ImageOutput
 
 from .. import create_images_group
 
-
-class TextAsImageFont(Enum):
-    ROBOTO_REGULAR = "roboto_regular"
-    ROBOTO_BOLD = "roboto_bold"
-    ROBOTO_ITALIC = "roboto_italic"
-    ROBOTO_BOLD_ITALIC = "roboto_bold_italic"
-    ROBOTO_MONO_REGULAR = "roboto_mono_regular"
-    ROBOTO_MONO_BOLD = "roboto_mono_bold"
-    ROBOTO_MONO_ITALIC = "roboto_mono_italic"
-    ROBOTO_MONO_BOLD_ITALIC = "roboto_mono_bold_italic"
-    OPEN_SANS_REGULAR = "open_sans_regular"
-    OPEN_SANS_BOLD = "open_sans_bold"
-    OPEN_SANS_ITALIC = "open_sans_italic"
-    OPEN_SANS_BOLD_ITALIC = "open_sans_bold_italic"
-
-
-TEXT_AS_IMAGE_FONTS = {
-    TextAsImageFont.ROBOTO_REGULAR: {
-        "label": "Roboto",
-        "path": "Roboto/Roboto-Regular.ttf",
-    },
-    TextAsImageFont.ROBOTO_BOLD: {
-        "label": "Roboto Bold",
-        "path": "Roboto/Roboto-Bold.ttf",
-    },
-    TextAsImageFont.ROBOTO_ITALIC: {
-        "label": "Roboto Italic",
-        "path": "Roboto/Roboto-Italic.ttf",
-    },
-    TextAsImageFont.ROBOTO_BOLD_ITALIC: {
-        "label": "Roboto Bold Italic",
-        "path": "Roboto/Roboto-BoldItalic.ttf",
-    },
-    TextAsImageFont.ROBOTO_MONO_REGULAR: {
-        "label": "Roboto Mono",
-        "path": "RobotoMono/RobotoMono-Regular.ttf",
-    },
-    TextAsImageFont.ROBOTO_MONO_BOLD: {
-        "label": "Roboto Mono Bold",
-        "path": "RobotoMono/RobotoMono-Bold.ttf",
-    },
-    TextAsImageFont.ROBOTO_MONO_ITALIC: {
-        "label": "Roboto Mono Italic",
-        "path": "RobotoMono/RobotoMono-Italic.ttf",
-    },
-    TextAsImageFont.ROBOTO_MONO_BOLD_ITALIC: {
-        "label": "Roboto Mono Bold Italic",
-        "path": "RobotoMono/RobotoMono-BoldItalic.ttf",
-    },
-    TextAsImageFont.OPEN_SANS_REGULAR: {
-        "label": "Open Sans",
-        "path": "OpenSans/OpenSans-Regular.ttf",
-    },
-    TextAsImageFont.OPEN_SANS_BOLD: {
-        "label": "Open Sans Bold",
-        "path": "OpenSans/OpenSans-Bold.ttf",
-    },
-    TextAsImageFont.OPEN_SANS_ITALIC: {
-        "label": "Open Sans Italic",
-        "path": "OpenSans/OpenSans-Italic.ttf",
-    },
-    TextAsImageFont.OPEN_SANS_BOLD_ITALIC: {
-        "label": "Open Sans Bold Italic",
-        "path": "OpenSans/OpenSans-BoldItalic.ttf",
-    },
-}
-
-TEXT_AS_IMAGE_FONT_LABELS = {
-    key: TEXT_AS_IMAGE_FONTS[key]["label"] for key in TextAsImageFont
-}
+TEXT_AS_IMAGE_FONT_PATH = [
+    [
+        "Roboto/Roboto-Regular.ttf",
+        "Roboto/Roboto-Italic.ttf",
+    ],
+    ["Roboto/Roboto-Bold.ttf", "Roboto/Roboto-BoldItalic.ttf"],
+]
 
 
 class TextAsImageAlignment(Enum):
@@ -158,16 +101,12 @@ TEXT_AS_IMAGE_X_Y_REF_FACTORS = {
 @create_images_group.register(
     schema_id="chainner:image:text_as_image",
     name="Text As Image",
-    description="Create an image using any text and a chosen font.",
+    description="Create an image using any text.",
     icon="MdTextFields",
     inputs=[
         TextInput("Text", multiline=True),
-        EnumInput(
-            TextAsImageFont,
-            label="Font",
-            option_labels=TEXT_AS_IMAGE_FONT_LABELS,
-            default=TextAsImageFont.ROBOTO_REGULAR,
-        ),
+        BoolInput("Bold", default=False),
+        BoolInput("Italic", default=False),
         ColorInput(channels=[1, 3], default=Color.bgr((0, 0, 0))),
         EnumInput(
             TextAsImageAlignment,
@@ -202,9 +141,9 @@ TEXT_AS_IMAGE_X_Y_REF_FACTORS = {
         ImageOutput(
             image_type="""
                 Image {
-                    width: Input4 & uint,
-                    height: Input5 & uint,
-                    channels: Input2.channels
+                    width: Input5 & uint,
+                    height: Input6 & uint,
+                    channels: Input3.channels
                 }
                 """,
             assume_normalized=True,
@@ -213,16 +152,18 @@ TEXT_AS_IMAGE_X_Y_REF_FACTORS = {
 )
 def text_as_image_node(
     text: str,
-    font_name: TextAsImageFont,
-    font_color: Color,
+    bold: bool,
+    italic: bool,
+    color: Color,
     alignment: TextAsImageAlignment,
     width: int,
     height: int,
     position: TextAsImagePosition,
 ) -> np.ndarray:
+    path = TEXT_AS_IMAGE_FONT_PATH[int(bold)][int(italic)]
     font_path = os.path.join(
         os.path.dirname(sys.modules["__main__"].__file__),  # type: ignore
-        f"fonts/{TEXT_AS_IMAGE_FONTS[font_name]['path']}",  # type: ignore
+        f"fonts/{path}",  # type: ignore
     )
 
     lines = text.split("\n")
@@ -241,12 +182,12 @@ def text_as_image_node(
     h_text *= line_count
 
     # Text color
-    color = 255 * np.array(font_color.value)
-    color = tuple(color.astype("uint8"))
+    ink = 255 * np.array(color.value)
+    ink = tuple(ink.astype("uint8"))
 
     # Create a PIL image to add text
-    if font_color.channels == 1:
-        color = color[0]
+    if color.channels == 1:
+        ink = ink[0]
         pil_image = Image.new("L", (width, height), 255)
     else:
         pil_image = Image.new("RGBA", (width, height), (255, 0, 0, 0))
@@ -261,11 +202,11 @@ def text_as_image_node(
         font=font,
         anchor="mm",
         align=alignment.value,
-        fill=color,
+        fill=ink,
     )
 
     img = normalize(np.array(pil_image))
-    if font_color.channels != 1:
+    if color.channels != 1:
         img = convert_to_BGRA(img, img.shape[2])
 
     return img
