8226c85e5adb52ab82244eab4dbf655f30390b6e
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-09-23 15:32:14 -0400

    Update backend/src/nodes/impl/cas.py

--- backend/src/nodes/impl/cas.py
+++ backend/src/nodes/impl/cas.py
@@ -37,7 +37,7 @@ def create_cas_mask(img: np.ndarray, kernel, bias: float = 2) -> np.ndarray:
     min_l = cv2.erode(l, kernel)
     max_l = cv2.dilate(l, kernel)
     min_d = np.minimum(1.0 - max_l, min_l, out=min_l)  # type: ignore
-    max_l += 1e-8 # type: ignore
+    max_l += 1e-8  # type: ignore
     min_d /= max_l
     mask = min_d
     if bias != 1:
