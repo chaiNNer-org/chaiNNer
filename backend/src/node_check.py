19f0cbf69e0cc4004ef77b8b9fe9ee6ba0755950
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-10-14 12:01:54 -0400

    Update backend/src/node_check.py
    Co-authored-by: Michael Schmidt <mitchi5000.ms@googlemail.com>

--- backend/src/node_check.py
+++ backend/src/node_check.py
@@ -152,7 +152,7 @@ def validate_return_type(return_type: _Ty, outputs: list[BaseOutput]):
                 f"Return type '{return_type}' must be a subset of '{o.associated_type}'"
             )
     else:
-        if not (str(return_type).startswith("typing.Tuple[")):
+        if not str(return_type).startswith("typing.Tuple["):
             raise CheckFailedError(
                 f"Return type '{return_type}' must be a tuple because there are multiple outputs"
             )
