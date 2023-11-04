2ff0ae3059b32b72105e30a43860b539cb32a487
Joey Ballentine <joeyjballentine@gmail.com>
2023-11-04 02:27:47 -0400

    Add some more commented out rules we should consider enabling

--- backend/src/nodes/impl/pytorch/utils.py
+++ backend/src/nodes/impl/pytorch/utils.py
@@ -41,7 +41,7 @@ def norm(x: Tensor):
 def np2tensor(
     img: np.ndarray,
     bgr2rgb=True,
-    data_range=1.0,  # pylint: disable=unused-argument
+    data_range=1.0,  # noqa: ARG001
     normalize=False,
     change_range=True,
     add_batch=True,
