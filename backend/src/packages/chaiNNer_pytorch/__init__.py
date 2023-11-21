6825f4c694732c6ba6d93be76bc408a93a5c949d
Joey Ballentine <joeyjballentine@gmail.com>
2023-11-21 00:35:56 -0500

    update spandrel

--- backend/src/packages/chaiNNer_pytorch/__init__.py
+++ backend/src/packages/chaiNNer_pytorch/__init__.py
@@ -95,7 +95,7 @@ package = add_package(
         Dependency(
             display_name="Spandrel",
             pypi_name="spandrel",
-            version="0.0.2",
+            version="0.0.3",
             size_estimate=180.7 * KB,
         ),
     ],
