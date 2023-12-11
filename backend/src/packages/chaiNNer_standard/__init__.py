60de534e7a0454921ce1e7ecba3e1223245fd540
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-12-11 10:53:39 +0100

    Fix resize for non-C-style arrays

--- backend/src/packages/chaiNNer_standard/__init__.py
+++ backend/src/packages/chaiNNer_standard/__init__.py
@@ -83,7 +83,7 @@ package = add_package(
         Dependency(
             display_name="ChaiNNer Extensions",
             pypi_name="chainner_ext",
-            version="0.3.8",
+            version="0.3.9",
             size_estimate=2.0 * MB,
         ),
     ],
