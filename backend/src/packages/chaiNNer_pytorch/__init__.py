c85763ecf473e6c9b06d3fbc8ab310d8aae92d64
Joey Ballentine <joeyjballentine@gmail.com>
2023-12-29 11:02:23 -0500

    Use cpu specifier for cpu pytorch

--- backend/src/packages/chaiNNer_pytorch/__init__.py
+++ backend/src/packages/chaiNNer_pytorch/__init__.py
@@ -30,13 +30,13 @@ def get_pytorch():
             Dependency(
                 display_name="PyTorch",
                 pypi_name="torch",
-                version="2.1.1",
+                version="2.1.2",
                 size_estimate=55.8 * MB,
             ),
             Dependency(
                 display_name="TorchVision",
                 pypi_name="torchvision",
-                version="0.16.1",
+                version="0.16.2",
                 size_estimate=1.3 * MB,
             ),
         ]
@@ -45,24 +45,16 @@ def get_pytorch():
             Dependency(
                 display_name="PyTorch",
                 pypi_name="torch",
-                version="2.1.1+cu121" if nvidia_is_available else "2.1.1",
+                version="2.1.2+cu121" if nvidia_is_available else "2.1.2+cpu",
                 size_estimate=2 * GB if nvidia_is_available else 140 * MB,
-                extra_index_url=(
-                    "https://download.pytorch.org/whl/cu121"
-                    if nvidia_is_available
-                    else None
-                ),
+                extra_index_url=("https://download.pytorch.org/whl/cu121"),
             ),
             Dependency(
                 display_name="TorchVision",
                 pypi_name="torchvision",
-                version="0.16.1+cu121" if nvidia_is_available else "0.16.1",
+                version="0.16.2+cu121" if nvidia_is_available else "0.16.2+cpu",
                 size_estimate=2 * MB if nvidia_is_available else 800 * KB,
-                extra_index_url=(
-                    "https://download.pytorch.org/whl/cu121"
-                    if nvidia_is_available
-                    else None
-                ),
+                extra_index_url=("https://download.pytorch.org/whl/cu121"),
             ),
         ]
 
