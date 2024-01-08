5afa666ccbfef788551b788154fc61453a3d55cc
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2024-01-07 21:23:36 -0500

    Allow glob negation in image iterator (#2432)
    Allow glob negation

--- backend/src/packages/chaiNNer_standard/image/batch_processing/load_images.py
+++ backend/src/packages/chaiNNer_standard/image/batch_processing/load_images.py
@@ -31,7 +31,7 @@ def extension_filter(lst: list[str]) -> str:
 def list_glob(directory: str, globexpr: str, ext_filter: list[str]) -> list[str]:
     extension_expr = extension_filter(ext_filter)
 
-    flags = glob.EXTGLOB | glob.BRACE | glob.GLOBSTAR
+    flags = glob.EXTGLOB | glob.BRACE | glob.GLOBSTAR | glob.NEGATE
 
     filtered = glob.globfilter(
         glob.iglob(globexpr, root_dir=directory, flags=flags),
