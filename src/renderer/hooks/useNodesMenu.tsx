171add76a631014155fec22244afe0d47eaa99bf
Joey Ballentine <joeyjballentine@gmail.com>
2024-01-16 01:02:39 -0500

    Add "Copy" to context menus

--- src/renderer/hooks/useNodesMenu.tsx
+++ src/renderer/hooks/useNodesMenu.tsx
@@ -1,9 +1,10 @@
 import { CloseIcon, CopyIcon, DeleteIcon } from '@chakra-ui/icons';
-import { MenuItem, MenuList } from '@chakra-ui/react';
-import { Node } from 'reactflow';
+import { MenuDivider, MenuItem, MenuList } from '@chakra-ui/react';
+import { Node, useReactFlow } from 'reactflow';
 import { useContext } from 'use-context-selector';
-import { NodeData } from '../../common/common-types';
+import { EdgeData, NodeData } from '../../common/common-types';
 import { GlobalContext } from '../contexts/GlobalNodeState';
+import { copyToClipboard } from '../helpers/copyAndPaste';
 import { UseContextMenu, useContextMenu } from './useContextMenu';
 
 export const useNodesMenu = (nodes: Node<NodeData>[]): UseContextMenu => {
@@ -11,8 +12,18 @@ export const useNodesMenu = (nodes: Node<NodeData>[]): UseContextMenu => {
 
     const nodeIds = nodes.map((n) => n.id);
 
+    const { getNodes, getEdges } = useReactFlow<NodeData, EdgeData>();
+
     return useContextMenu(() => (
         <MenuList className="nodrag">
+            <MenuItem
+                icon={<CopyIcon />}
+                onClick={() => {
+                    copyToClipboard(getNodes(), getEdges());
+                }}
+            >
+                Copy
+            </MenuItem>
             <MenuItem
                 icon={<CopyIcon />}
                 onClick={() => {
@@ -21,6 +32,7 @@ export const useNodesMenu = (nodes: Node<NodeData>[]): UseContextMenu => {
             >
                 Duplicate
             </MenuItem>
+            <MenuDivider />
             <MenuItem
                 icon={<CloseIcon />}
                 onClick={() => {
