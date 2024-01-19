2fd519e3f9eccfda5d5fdbbb45de27ecbf95e5d9
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-01-19 14:41:00 +0100

    Make Save Video input names consistent with Save Image

--- backend/src/packages/chaiNNer_standard/image/video_frames/save_video.py
+++ backend/src/packages/chaiNNer_standard/image/video_frames/save_video.py
@@ -84,8 +84,8 @@ class Writer:
     icon="MdVideoCameraBack",
     inputs=[
         ImageInput("Image Sequence", channels=3),
-        DirectoryInput("Output Video Directory", has_handle=True),
-        TextInput("Output Video Name"),
+        DirectoryInput("Directory", has_handle=True),
+        TextInput("Video Name"),
         VideoEncoderDropdown().with_docs("Encoder").with_id(3),
         if_enum_group(3, VideoEncoder.H264)(
             VideoH264ContainerDropdown().with_docs("Container").with_id(4)
