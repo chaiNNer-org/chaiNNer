61ab0c9b7e0ca43e2aa2f9e89f2ebf89f97d37d0
MrJs <30782821+mrjschulte@users.noreply.github.com>
2024-02-03 09:36:58 -0800

    Create "Multiply" node (#2540)
    * Add multiply.py node
    
    This node will allow a user to Multiply the input pixels in an image by a set value.
    
    * Update multiply.py
    
    Clean up typos and extra comments
    
    * Update multiply.py
    
    More lint fixes
    
    * Update multiply.py
    
    reword float32 cast to just a float
    
    * Update multiply.py
    
    more linting

---
+++ backend/src/packages/chaiNNer_standard/image_adjustment/adjustments/multiply.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import numpy as np
+
+from nodes.properties.inputs import ImageInput, SliderInput
+from nodes.properties.outputs import ImageOutput
+
+from .. import adjustments_group
+
+
+@adjustments_group.register(
+    schema_id="chainner:image:multiply",
+    description="Multiply all channels in an image by a value.",
+    name="Multiply",
+    icon="ImBrightnessContrast",
+    inputs=[
+        ImageInput(),
+        SliderInput(
+            "Multiply",
+            minimum=0.0,
+            maximum=4.0,
+            default=1.0,
+            precision=4,
+            controls_step=0.0001,
+            scale="log",
+        ),
+    ],
+    outputs=[ImageOutput(image_type="Input0")],
+)
+def multiply_node(img: np.ndarray, mult: float) -> np.ndarray:
+    if mult == 1.0:
+        return img
+
+    img = img * mult
+
+    return img
