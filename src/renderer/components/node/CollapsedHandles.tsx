0afdafb1619119605da761819fdc87a99101023a
Joey Ballentine <joeyjballentine@gmail.com>
2024-02-07 02:27:59 -0500

    Fix input type colors in collapsed node

--- src/renderer/components/node/CollapsedHandles.tsx
+++ src/renderer/components/node/CollapsedHandles.tsx
@@ -1,13 +1,86 @@
 import { NeverType } from '@chainner/navi';
 import { Box } from '@chakra-ui/react';
-import { memo } from 'react';
-import { Handle, Position } from 'reactflow';
-import { useContextSelector } from 'use-context-selector';
-import { stringifySourceHandle, stringifyTargetHandle } from '../../../common/util';
+import { memo, useMemo } from 'react';
+import { Handle, Node, Position, useReactFlow } from 'reactflow';
+import { useContext, useContextSelector } from 'use-context-selector';
+import { InputId, NodeData } from '../../../common/common-types';
+import {
+    parseSourceHandle,
+    parseTargetHandle,
+    stringifySourceHandle,
+    stringifyTargetHandle,
+} from '../../../common/util';
 import { BackendContext } from '../../contexts/BackendContext';
-import { getTypeAccentColors } from '../../helpers/accentColors';
+import { GlobalVolatileContext } from '../../contexts/GlobalNodeState';
+import { defaultColor, getTypeAccentColors } from '../../helpers/accentColors';
 import { NodeState } from '../../helpers/nodeState';
 import { getBackground } from '../Handle';
+
+interface InputHandleProps {
+    nodeId: string;
+    inputId: InputId;
+    isIterated: boolean;
+}
+
+const InputHandle = memo(({ nodeId, isIterated, inputId }: InputHandleProps) => {
+    const { edgeChanges, typeState } = useContext(GlobalVolatileContext);
+    const { getEdges, getNode } = useReactFlow();
+
+    const connectedEdge = useMemo(() => {
+        return getEdges().find(
+            (e) => e.target === nodeId && parseTargetHandle(e.targetHandle!).inputId === inputId
+        );
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [edgeChanges, getEdges, nodeId, inputId]);
+
+    const { functionDefinitions } = useContext(BackendContext);
+
+    const sourceTypeColor = useMemo(() => {
+        if (connectedEdge) {
+            const sourceNode: Node<NodeData> | undefined = getNode(connectedEdge.source);
+            const sourceOutputId = parseSourceHandle(connectedEdge.sourceHandle!).outputId;
+            if (sourceNode) {
+                const sourceDef = functionDefinitions.get(sourceNode.data.schemaId);
+                if (!sourceDef) {
+                    return defaultColor;
+                }
+                const sourceType =
+                    typeState.functions.get(sourceNode.id)?.outputs.get(sourceOutputId) ??
+                    sourceDef.outputDefaults.get(sourceOutputId);
+                if (!sourceType) {
+                    return defaultColor;
+                }
+                return getTypeAccentColors(sourceType)[0];
+            }
+            return defaultColor;
+        }
+        return null;
+    }, [connectedEdge, functionDefinitions, typeState, getNode]);
+
+    const handleId = stringifyTargetHandle({ nodeId, inputId });
+
+    return (
+        <Box
+            h="6px"
+            key={handleId}
+            mr="auto"
+            position="relative"
+            w="6px"
+        >
+            <Handle
+                className="input-handle"
+                id={handleId}
+                isConnectable={false}
+                position={Position.Left}
+                style={{
+                    borderColor: sourceTypeColor || defaultColor,
+                    borderRadius: isIterated ? '10%' : '50%',
+                }}
+                type="target"
+            />
+        </Box>
+    );
+});
 
 interface CollapsedHandlesProps {
     nodeState: NodeState;
@@ -40,35 +113,14 @@ export const CollapsedHandles = memo(({ nodeState }: CollapsedHandlesProps) => {
                             return null;
                         }
 
-                        const connectableType =
-                            functionDefinition?.inputConvertibleDefaults.get(input.id) ??
-                            NeverType.instance;
-                        const handleColors = getTypeAccentColors(connectableType);
-
                         const isIterated = iteratedInputs.has(input.id);
 
-                        const handleId = stringifyTargetHandle({ nodeId, inputId: input.id });
-
                         return (
-                            <Box
-                                h="6px"
-                                key={handleId}
-                                mr="auto"
-                                position="relative"
-                                w="6px"
-                            >
-                                <Handle
-                                    className="input-handle"
-                                    id={handleId}
-                                    isConnectable={false}
-                                    position={Position.Left}
-                                    style={{
-                                        borderColor: getBackground(handleColors),
-                                        borderRadius: isIterated ? '10%' : '50%',
-                                    }}
-                                    type="target"
-                                />
-                            </Box>
+                            <InputHandle
+                                inputId={input.id}
+                                isIterated={isIterated}
+                                nodeId={nodeId}
+                            />
                         );
                     })}
                 </Box>
