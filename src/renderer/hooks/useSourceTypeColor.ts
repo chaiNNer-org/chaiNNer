5b8e950fe7475189f9de0c9dc5ff60a49ca8aa27
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-02-08 16:59:55 +0100

    Refactor `useSourceTypeColor` and some parts of handles

--- src/renderer/hooks/useSourceTypeColor.ts
+++ src/renderer/hooks/useSourceTypeColor.ts
@@ -1,36 +1,34 @@
 import { useMemo } from 'react';
 import { Node, useReactFlow } from 'reactflow';
 import { useContext } from 'use-context-selector';
-import { InputId, NodeData } from '../../common/common-types';
-import { parseSourceHandle, parseTargetHandle } from '../../common/util';
+import { NodeData } from '../../common/common-types';
+import { parseSourceHandle } from '../../common/util';
 import { BackendContext } from '../contexts/BackendContext';
 import { GlobalVolatileContext } from '../contexts/GlobalNodeState';
 import { defaultColor, getTypeAccentColors } from '../helpers/accentColors';
 
-export const useSourceTypeColor = (nodeId: string, inputId: InputId) => {
+export const useSourceTypeColor = (targetHandle: string) => {
     const { functionDefinitions } = useContext(BackendContext);
     const { edgeChanges, typeState } = useContext(GlobalVolatileContext);
     const { getEdges, getNode } = useReactFlow();
 
-    const connectedEdge = useMemo(() => {
-        return getEdges().find(
-            (e) => e.target === nodeId && parseTargetHandle(e.targetHandle!).inputId === inputId
-        );
+    const sourceHandle = useMemo(() => {
+        return getEdges().find((e) => e.targetHandle === targetHandle)?.sourceHandle;
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [edgeChanges, getEdges, nodeId, inputId]);
+    }, [edgeChanges, getEdges, targetHandle]);
 
     const sourceTypeColor = useMemo(() => {
-        if (connectedEdge) {
-            const sourceNode: Node<NodeData> | undefined = getNode(connectedEdge.source);
-            const sourceOutputId = parseSourceHandle(connectedEdge.sourceHandle!).outputId;
+        if (sourceHandle) {
+            const source = parseSourceHandle(sourceHandle);
+            const sourceNode: Node<NodeData> | undefined = getNode(source.nodeId);
             if (sourceNode) {
                 const sourceDef = functionDefinitions.get(sourceNode.data.schemaId);
                 if (!sourceDef) {
                     return defaultColor;
                 }
                 const sourceType =
-                    typeState.functions.get(sourceNode.id)?.outputs.get(sourceOutputId) ??
-                    sourceDef.outputDefaults.get(sourceOutputId);
+                    typeState.functions.get(source.nodeId)?.outputs.get(source.outputId) ??
+                    sourceDef.outputDefaults.get(source.outputId);
                 if (!sourceType) {
                     return defaultColor;
                 }
@@ -39,7 +37,7 @@ export const useSourceTypeColor = (nodeId: string, inputId: InputId) => {
             return defaultColor;
         }
         return null;
-    }, [connectedEdge, functionDefinitions, typeState, getNode]);
+    }, [sourceHandle, functionDefinitions, typeState, getNode]);
 
     return sourceTypeColor;
 };
