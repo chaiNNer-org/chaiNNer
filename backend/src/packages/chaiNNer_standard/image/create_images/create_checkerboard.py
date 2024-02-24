9c1d5ccebe29af9061cea8683d01c7b88d61c2ac
MrJs <30782821+mrjschulte@users.noreply.github.com>
2024-02-23 20:24:31 -0800

    Create "Checkerboard" node
    This node creates a checkerboard with user definable colors and size for the squares.

---
+++ backend/src/packages/chaiNNer_standard/image/create_images/create_checkerboard.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import numpy as np
+
+import navi
+from nodes.impl.color.color import Color
+from nodes.properties.inputs import ColorInput, NumberInput
+from nodes.properties.outputs import ImageOutput
+
+from .. import create_images_group
+
+
+@create_images_group.register(
+    schema_id="chainner:image:create_checkerboard",
+    name="Create Checkerboard",
+    description="Create a Checkerboard of specified dimensions filled with the given colors for the squares.",
+    icon="MdBorderAll",
+    inputs=[
+        NumberInput("Width", minimum=1, unit="px", default=1024),
+        NumberInput("Height", minimum=1, unit="px", default=1024),
+        ColorInput(
+            "Color 1", channels=[3], default=Color.bgr((0.75, 0.75, 0.75))
+        ).with_id(2),
+        ColorInput(
+            "Color 2", channels=[3], default=Color.bgr((0.35, 0.35, 0.35))
+        ).with_id(3),
+        NumberInput("Square Size", minimum=1, default=32),
+    ],
+    outputs=[
+        ImageOutput(
+            image_type=navi.Image(
+                width="Input0",
+                height="Input1",
+                channels=3,
+            ),
+        )
+    ],
+)
+def create_checkerboard_node(
+    width: float,
+    height: float,
+    color_1: Color,
+    color_2: Color,
+    square_size: float,
+) -> np.ndarray:
+    img = np.zeros(
+        (height, width, 3), dtype=np.float32
+    )  # Create a new buffer with all zeros
+
+    # Determine the number of squares in each direction
+    num_cols = (width + square_size - 1) // square_size
+    num_rows = (height + square_size - 1) // square_size
+
+    # Fill the checkerboard with alternating squares
+    for i in range(num_rows):
+        for j in range(num_cols):
+            if (i + j) % 2 == 0:
+                img[
+                    max(0, height - (i + 1) * square_size) : max(
+                        0, height - i * square_size
+                    ),
+                    j * square_size : min(width, (j + 1) * square_size),
+                ] = color_1.value
+            else:
+                img[
+                    max(0, height - (i + 1) * square_size) : max(
+                        0, height - i * square_size
+                    ),
+                    j * square_size : min(width, (j + 1) * square_size),
+                ] = color_2.value
+
+    return img
