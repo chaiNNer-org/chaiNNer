03677e6cfe9fb098e090486393461961ce5eca4e
Michael Schmidt <mitchi5000.ms@googlemail.com>
2024-02-26 18:59:26 +0100

    Refactor video loading (#2616)
    * Refactor video loading
    
    * Fixed bug in limit

--- backend/src/packages/chaiNNer_standard/image/video_frames/load_video.py
+++ backend/src/packages/chaiNNer_standard/image/video_frames/load_video.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
-import os
 from pathlib import Path
 from typing import Any
 
-import ffmpeg
 import numpy as np
 
 from api import Iterator, IteratorOutputInfo
 from nodes.groups import Condition, if_group
+from nodes.impl.video import VideoLoader
 from nodes.properties.inputs import BoolInput, NumberInput, VideoFileInput
 from nodes.properties.outputs import (
     AudioStreamOutput,
@@ -20,9 +19,6 @@ from nodes.properties.outputs import (
 from nodes.utils.utils import split_file_path
 
 from .. import video_frames_group
-
-ffmpeg_path = os.environ.get("STATIC_FFMPEG_PATH", "ffmpeg")
-ffprobe_path = os.environ.get("STATIC_FFPROBE_PATH", "ffprobe")
 
 
 @video_frames_group.register(
@@ -52,7 +48,7 @@ ffprobe_path = os.environ.get("STATIC_FFPROBE_PATH", "ffprobe")
         ).with_docs("A counter that starts at 0 and increments by 1 for each frame."),
         DirectoryOutput("Video Directory", of_input=0),
         FileNameOutput("Name", of_input=0),
-        NumberOutput("FPS"),
+        NumberOutput("FPS", output_type="0.."),
         AudioStreamOutput(),
     ],
     iterator_outputs=IteratorOutputInfo(outputs=[0, 1]),
@@ -65,76 +61,24 @@ def load_video_node(
 ) -> tuple[Iterator[tuple[np.ndarray, int]], Path, str, float, Any]:
     video_dir, video_name, _ = split_file_path(path)
 
-    ffmpeg_reader = (
-        ffmpeg.input(path)
-        .output(
-            "pipe:",
-            format="rawvideo",
-            pix_fmt="bgr24",
-            sws_flags="lanczos+accurate_rnd+full_chroma_int+full_chroma_inp+bitexact",
-            loglevel="error",
-        )
-        .run_async(pipe_stdout=True, cmd=ffmpeg_path)
-    )
-
-    probe = ffmpeg.probe(path, cmd=ffprobe_path)
-    video_format = probe.get("format", None)
-    if video_format is None:
-        raise RuntimeError("Failed to get video format. Please report.")
-    video_stream = next(
-        (stream for stream in probe["streams"] if stream["codec_type"] == "video"),
-        None,
-    )
-
-    if video_stream is None:
-        raise RuntimeError("No video stream found in file")
-
-    width = video_stream.get("width", None)
-    if width is None:
-        raise RuntimeError("No width found in video stream")
-    width = int(width)
-    height = video_stream.get("height", None)
-    if height is None:
-        raise RuntimeError("No height found in video stream")
-    height = int(height)
-    fps = video_stream.get("r_frame_rate", None)
-    if fps is None:
-        raise RuntimeError("No fps found in video stream")
-    fps = int(fps.split("/")[0]) / int(fps.split("/")[1])
-    frame_count = video_stream.get("nb_frames", None)
-    if frame_count is None:
-        duration = video_stream.get("duration", None)
-        if duration is None:
-            duration = video_format.get("duration", None)
-        if duration is not None:
-            frame_count = float(duration) * fps
-        else:
-            raise RuntimeError(
-                "No frame count or duration found in video stream. Unable to determine video length. Please report."
-            )
-    frame_count = int(frame_count)
+    loader = VideoLoader(path)
+    frame_count = loader.metadata.frame_count
     if use_limit:
         frame_count = min(frame_count, limit)
 
-    audio_stream = ffmpeg.input(path).audio
+    audio_stream = loader.get_audio_stream()
 
     def iterator():
-        index = 0
-        while True:
-            if use_limit and index >= limit:
+        for index, frame in enumerate(loader.stream_frames()):
+            yield frame, index
+
+            if use_limit and index + 1 >= limit:
                 break
-            in_bytes = ffmpeg_reader.stdout.read(width * height * 3)
-            if not in_bytes:
-                print("Can't receive frame (stream end?). Exiting ...")
-                break
-            in_frame = np.frombuffer(in_bytes, np.uint8).reshape([height, width, 3])
-            yield in_frame, index
-            index += 1
 
     return (
         Iterator.from_iter(iter_supplier=iterator, expected_length=frame_count),
         video_dir,
         video_name,
-        fps,
+        loader.metadata.fps,
         audio_stream,
     )
