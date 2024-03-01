46542f5aab5ea7351c432e576a18c56dcd2b1701
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-03-01 20:40:48 +0100

    lint

--- backend/src/nodes/impl/noise_functions/noise_generator.py
+++ backend/src/nodes/impl/noise_functions/noise_generator.py
@@ -5,5 +5,4 @@ import numpy as np
 
 class NoiseGenerator(ABC):
     @abstractmethod
-    def evaluate(self, points: np.ndarray) -> np.ndarray:
-        ...
+    def evaluate(self, points: np.ndarray) -> np.ndarray: ...
