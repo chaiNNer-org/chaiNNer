ee86b013451094f9b3466b54ae6953842c7e9f98
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2024-03-08 11:38:40 -0500

    Avoid restarting backend if no deps installed (#2650)
    * better implementation
    
    * we dont need this anymore

--- backend/src/server_host.py
+++ backend/src/server_host.py
@@ -194,7 +194,8 @@ async def import_packages(
             )
             for dep in dependencies
         ]
-        await install_dependencies(dep_info, update_progress_cb, logger)
+        num_installed = await install_dependencies(dep_info, update_progress_cb, logger)
+        return num_installed
 
     packages = await worker.get_packages()
 
@@ -217,10 +218,10 @@ async def import_packages(
             if dep.auto_update and is_installed:
                 to_install.append(dep)
 
-    if len(to_install) > 0:
-        try:
-            await install_deps(to_install)
+    try:
+        num_installed = await install_deps(to_install)
 
+        if num_installed > 0:
             flags = []
             if config.error_on_failed_node:
                 flags.append("--error-on-failed-node")
@@ -229,10 +230,10 @@ async def import_packages(
                 flags.append("--close-after-start")
 
             await worker.restart(flags)
-        except Exception as ex:
-            logger.error(f"Error installing dependencies: {ex}", exc_info=True)
-            if config.close_after_start:
-                raise ValueError("Error installing dependencies") from ex
+    except Exception as ex:
+        logger.error(f"Error installing dependencies: {ex}", exc_info=True)
+        if config.close_after_start:
+            raise ValueError("Error installing dependencies") from ex
 
     logger.info("Done checking dependencies...")
 
