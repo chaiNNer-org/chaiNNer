90b5e1eb515fcbc5f2458917eef960f431becc64
Joey Ballentine <joeyjballentine@gmail.com>
2024-04-04 19:15:12 -0400

    Disable menu while backend not ready

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -79,6 +79,7 @@ export const Main = memo(() => {
     }, []);
 
     useEffect(() => {
+        ipcRenderer.send(isBackendReady ? 'enable-menu' : 'disable-menu');
         if (isBackendReady) {
             setOverallProgress(1);
         }
