b3722d4413ddf3420c92d0a6a3b2899aaca4833f
Michael Schmidt <mitchi5000.ms@googlemail.com>
2024-04-09 17:00:47 +0200

    Organize adj category (#2776)
    * Reorganize nodes in Image Adjustment
    
    * Oops

--- backend/src/packages/chaiNNer_standard/image_adjustment/__init__.py
+++ backend/src/packages/chaiNNer_standard/image_adjustment/__init__.py
@@ -2,3 +2,5 @@ from .. import image_adjustments_category
 
 adjustments_group = image_adjustments_category.add_node_group("Adjustments")
 threshold_group = image_adjustments_category.add_node_group("Threshold")
+gamma_group = image_adjustments_category.add_node_group("Gamma")
+arithmetic_group = image_adjustments_category.add_node_group("Arithmetic")
