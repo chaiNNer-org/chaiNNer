81fe7aabec67d636f7a16c9e5390f57d446b008f
tepete <pifroggimc@gmail.com>
2024-04-14 16:34:23 +0200

    removed commented out code

--- backend/src/packages/chaiNNer_pytorch/pytorch/processing/rife/warplayer.py
+++ backend/src/packages/chaiNNer_pytorch/pytorch/processing/rife/warplayer.py
@@ -1,9 +1,6 @@
 import torch
-import torch.nn as nn
 
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 backwarp_tenGrid = {}
-
 
 def warp(tenInput, tenFlow, device):
     k = (str(tenFlow.device), str(tenFlow.size()))
@@ -13,12 +10,11 @@ def warp(tenInput, tenFlow, device):
         tenVertical = torch.linspace(-1.0, 1.0, tenFlow.shape[2], device=device).view(
             1, 1, tenFlow.shape[2], 1).expand(tenFlow.shape[0], -1, -1, tenFlow.shape[3])
         backwarp_tenGrid[k] = torch.cat(
-            [tenHorizontal, tenVertical], 1).to(device)#.half()
+            [tenHorizontal, tenVertical], 1).to(device)
 
     tenFlow = torch.cat([tenFlow[:, 0:1, :, :] / ((tenInput.shape[3] - 1.0) / 2.0),
                          tenFlow[:, 1:2, :, :] / ((tenInput.shape[2] - 1.0) / 2.0)], 1)
 
     g = (backwarp_tenGrid[k] + tenFlow).permute(0, 2, 3, 1)
     tenOutput = torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bicubic', padding_mode='border', align_corners=True)
-    #tenOutput = tenOutput.clamp(min=0.0, max=1.0) #changes output, without seems better
     return tenOutput
