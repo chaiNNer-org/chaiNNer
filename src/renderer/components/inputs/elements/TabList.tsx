dad2dc7aa07bee7749e5bb74faac2a2bad037c24
raysit <18922396056@163.com>
2024-04-14 15:09:50 -0500

    issue #2734 update: removed onChange prop in 'Tabs'

--- src/renderer/components/inputs/elements/TabList.tsx
+++ src/renderer/components/inputs/elements/TabList.tsx
@@ -1,7 +1,6 @@
 import { TabList as ChakraTabList, Tab, TabIndicator, Tabs } from '@chakra-ui/react';
 import { memo, useEffect } from 'react';
 import { DropDownInput, InputSchemaValue } from '../../../../common/common-types';
-import { noop } from '../../../../common/util';
 
 export interface TabListProps {
     value: InputSchemaValue | undefined;
@@ -44,7 +43,6 @@ export const TabList = memo(({ value, onChange, reset, isDisabled, options }: Ta
             pt={1}
             size="sm"
             variant="unstyled"
-            onChange={noop}
         >
             <ChakraTabList
                 borderBottom="1px solid"
