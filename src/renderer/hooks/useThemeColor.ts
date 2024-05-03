9c637eb7b031a1f7f37b1b5e266391bb666593a2
Joey Ballentine <joeyjballentine@gmail.com>
2024-05-03 03:03:13 -0400

    Recompute node headers when theme color changes

--- src/renderer/hooks/useThemeColor.ts
+++ src/renderer/hooks/useThemeColor.ts
@@ -1,14 +1,17 @@
 import { useColorMode } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { lazy } from '../../common/util';
+import { useSettings } from '../contexts/SettingsContext';
 
 const light = lazy(() => getComputedStyle(document.documentElement));
 const dark = lazy(() => getComputedStyle(document.documentElement));
 
 export const useThemeColor = (name: `--${string}`): string => {
     const { colorMode } = useColorMode();
+    const { realTheme } = useSettings();
     return useMemo(() => {
         const styles = colorMode === 'dark' ? dark() : light();
         return styles.getPropertyValue(name).trim();
-    }, [colorMode, name]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [colorMode, name, realTheme]);
 };
