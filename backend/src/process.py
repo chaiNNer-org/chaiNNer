52e160e37d3611e85c64b950c988cafc40e1fa2a
Joey Ballentine <joeyjballentine@gmail.com>
2024-05-17 11:30:08 -0400

    replace with time.sleep(0)

--- backend/src/process.py
+++ backend/src/process.py
@@ -348,6 +348,7 @@ class _ExecutorNodeContext(NodeContext):
 
     @property
     def paused(self) -> bool:
+        time.sleep(0)
         return self.progress.paused
 
     def set_progress(self, progress: float) -> None:
