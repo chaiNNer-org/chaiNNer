5cc1189f62192c609b34afacff2e796c23b3e298
Joey Ballentine <joeyjballentine@gmail.com>
2024-06-15 16:06:19 -0400

    Add identity functions, use frozen sets

--- backend/tests/test_util.py
+++ backend/tests/test_util.py
@@ -1,11 +1,28 @@
+from __future__ import annotations
+
+from typing import TypeVar
+
 from util import combine_sets
+
+T = TypeVar("T")
+
+
+def freeze(list_of_sets: list[set[T]]) -> set[frozenset[T]]:
+    return {frozenset(x) for x in list_of_sets}
+
+
+S = TypeVar("S")
+
+
+def f_equal(a: list[set[S]], b: list[set[S]]) -> bool:
+    return freeze(a) == freeze(b)
 
 
 def test_combine_sets_one():
     test_sets = [{1, 2, 3}, {1, 4}, {5, 6}, {7, 8}]
     test_output = [{1, 2, 3, 4}, {5, 6}, {7, 8}]
     result = combine_sets(test_sets)
-    assert result == test_output
+    assert f_equal(result, test_output)
 
 
 def test_combine_sets_two():
@@ -20,7 +37,7 @@ def test_combine_sets_two():
     ]
     test_output = [{1, 2, 3, 4}, {5, 6, 7, 8, 9, 10}, {50, 51, 52}, {60, 61}]
     result = combine_sets(test_sets)
-    assert result == test_output
+    assert f_equal(result, test_output)
 
 
 def test_combine_sets_three():
@@ -35,13 +52,13 @@ def test_combine_sets_three():
         {60, 61},
     ]
     test_output = [
-        {34, 60, 61, 90},
         {1, 2, 3, 4, 87},
         {5, 6, 7, 8, 9, 10},
         {50, 51, 52},
+        {34, 60, 61, 90},
     ]
     result = combine_sets(test_sets)
-    assert result == test_output
+    assert f_equal(result, test_output)
 
 
 def test_combine_sets_four():
@@ -59,7 +76,7 @@ def test_combine_sets_four():
         {"x", "y", "z"},
     ]
     result = combine_sets(test_sets)
-    assert result == test_output
+    assert f_equal(result, test_output)
 
 
 def test_combine_sets_five():
@@ -91,4 +108,25 @@ def test_combine_sets_five():
         },
     ]
     result = combine_sets(test_sets)
-    assert result == test_output
+    assert f_equal(result, test_output)
+
+
+def test_combine_sets_identity_one():
+    test_sets = [{"a", "b", "c"}, {"d", "e", "f"}, {"g", "h", "i"}]
+    test_output = [{"a", "b", "c"}, {"d", "e", "f"}, {"g", "h", "i"}]
+    result = combine_sets(test_sets)
+    assert f_equal(result, test_output)
+
+
+def test_combine_sets_identity_two():
+    test_sets = [{"a", "b", "c", "d", "e", "f"}]
+    test_output = [{"a", "b", "c", "d", "e", "f"}]
+    result = combine_sets(test_sets)
+    assert f_equal(result, test_output)
+
+
+def test_combine_sets_identity_three():
+    test_sets = [{"a"}, {"b"}, {"c"}, {"d"}, {"e"}, {"f"}]
+    test_output = [{"a"}, {"b"}, {"c"}, {"d"}, {"e"}, {"f"}]
+    result = combine_sets(test_sets)
+    assert f_equal(result, test_output)
