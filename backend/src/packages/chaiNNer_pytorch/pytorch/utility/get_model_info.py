9771715bb44947888d87fec1b6af35194b227bfd
tumuyan <tumuyan@gmail.com>
2025-03-06 21:56:18 +0800

    Change get_model_scale node to get_model_info node

---
+++ backend/src/packages/chaiNNer_pytorch/pytorch/utility/get_model_info.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from spandrel import ModelDescriptor
+
+from nodes.properties.inputs import ModelInput
+from nodes.properties.outputs import NumberOutput
+from nodes.properties.outputs import TextOutput
+
+from .. import utility_group
+
+
+@utility_group.register(
+    schema_id="chainner:pytorch:model_dim",
+    name="Get Model Info",
+    description="""Returns the purpose, architecture and scale of a PyTorch model.""",
+    icon="BsRulers",
+    inputs=[ModelInput()],
+    outputs=[
+        NumberOutput("Scale", output_type="Input0.scale"),
+        TextOutput("Architecture", output_type="Input0.arch"),
+        TextOutput("Purpose", output_type="Input0.subType"),
+    ],
+)
+def get_model_info_node(model: ModelDescriptor) -> tuple[int, string, string]:
+    return model.scale, model.arch, model.subType
