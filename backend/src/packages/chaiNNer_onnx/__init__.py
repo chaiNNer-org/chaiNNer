55dec8fe206424ffb07d2b9d3e0db9f94b832cd4
Joey Ballentine <joeyjballentine@gmail.com>
2025-10-18 23:48:30 -0500

    update onnxruntime

--- backend/src/packages/chaiNNer_onnx/__init__.py
+++ backend/src/packages/chaiNNer_onnx/__init__.py
@@ -23,7 +23,7 @@ def get_onnx_runtime():
         return Dependency(
             display_name="ONNX Runtime (GPU)",
             pypi_name="onnxruntime-gpu",
-            version="1.19.2",
+            version="1.20.1",
             size_estimate=226 * MB,
             import_name="onnxruntime",
             extra_index_url="https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/",
@@ -32,14 +32,14 @@ def get_onnx_runtime():
         return Dependency(
             display_name="ONNX Runtime (DirectMl)",
             pypi_name="onnxruntime-directml",
-            version="1.19.2",
+            version="1.20.1",
             size_estimate=15 * MB,
         )
     else:
         return Dependency(
             display_name="ONNX Runtime",
             pypi_name="onnxruntime",
-            version="1.19.2",
+            version="1.20.1",
             size_estimate=13 * MB,
         )
 
