bb976b2e3366970508068d07dc886201f6479da1
Joey Ballentine <joeyjballentine@gmail.com>
2025-10-21 19:12:05 -0500

    Fix capitalization

--- src/renderer/components/SettingsModal.tsx
+++ src/renderer/components/SettingsModal.tsx
@@ -131,7 +131,7 @@ const AppearanceSettings = memo(() => {
             <DropdownSetting
                 setValue={(value) => setLanguage(value)}
                 setting={{
-                    label: t('settings.appearance.language.label', 'Language (Requires restart)'),
+                    label: t('settings.appearance.language.label', 'Language (requires restart)'),
                     description: t(
                         'settings.appearance.language.description',
                         "Choose the language for chaiNNer's interface."
