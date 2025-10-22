c159515df16055acec0d1c276b44eb64d1472044
Joey Ballentine <joeyjballentine@gmail.com>
2025-10-22 12:36:42 -0500

    Disable MacOS code signing

--- forge.config.js
+++ forge.config.js
@@ -97,14 +97,14 @@ const config = {
                 additionalDMGOptions: {
                     window: { size: { width: 660, height: 500 } },
                     filesystem: 'APFS',
-                    ...(process.argv.includes('--dry-run') || process.argv.includes('--no-sign')
-                        ? {}
-                        : {
-                              'code-sign': {
-                                  'signing-identity': process.env.APPLE_SIGNING_ID,
-                                  identifier: packageJson.appId,
-                              },
-                          }),
+                    // ...(process.argv.includes('--dry-run') || process.argv.includes('--no-sign')
+                    //     ? {}
+                    //     : {
+                    //           'code-sign': {
+                    //               'signing-identity': process.env.APPLE_SIGNING_ID,
+                    //               identifier: packageJson.appId,
+                    //           },
+                    //       }),
                 },
             },
         },
