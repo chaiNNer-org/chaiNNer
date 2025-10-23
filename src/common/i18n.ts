3557360d94371967e6fa5bf9ba80625695f7ae53
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2025-10-23 12:14:46 -0500

    Add support for more languages (#3180)
    * Add support for more languages
    
    * Fix import order
    
    * Add more languages
    
    * lint

--- src/common/i18n.ts
+++ src/common/i18n.ts
@@ -1,12 +1,38 @@
 import { InitOptions } from 'i18next';
+import ar from './locales/ar.json';
 import de from './locales/de.json';
 import en from './locales/en.json';
 import es from './locales/es.json';
+import fr from './locales/fr.json';
+import hi from './locales/hi.json';
+import it from './locales/it.json';
+import ja from './locales/ja.json';
+import ko from './locales/ko.json';
+import nl from './locales/nl.json';
+import pl from './locales/pl.json';
+import ptBR from './locales/pt-BR.json';
+import pt from './locales/pt.json';
+import ru from './locales/ru.json';
+import tr from './locales/tr.json';
+import zhCN from './locales/zh-CN.json';
 
 const resources = {
     en: { translation: en },
     es: { translation: es },
     de: { translation: de },
+    fr: { translation: fr },
+    it: { translation: it },
+    pt: { translation: pt },
+    ja: { translation: ja },
+    'zh-CN': { translation: zhCN },
+    ru: { translation: ru },
+    ko: { translation: ko },
+    nl: { translation: nl },
+    pl: { translation: pl },
+    tr: { translation: tr },
+    ar: { translation: ar },
+    hi: { translation: hi },
+    'pt-BR': { translation: ptBR },
 };
 
 export const DEFAULT_OPTIONS: InitOptions = {
